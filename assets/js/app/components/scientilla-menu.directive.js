bac875c816040fc0bb78569e7fce3de00a4f034b
iit-federico-bozzini <federico.bozzini@iit.it>
2015-11-25 16:19:18 +0100

    Menu moved to its own directive.

---
+++ assets/js/app/components/scientilla-menu.directive.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular.module('components')
+            .directive('scientillaMenu', scientillaMenu);
+    
+    function scientillaMenu() {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/scientillaMenu.html',
+            controller: scientillaMenuController,
+            controllerAs: 'vm',
+            scope: {
+            }
+        };
+    }
+    
+    function scientillaMenuController($scope, AuthService) {
+        var vm = this;
+
+        $scope.$on('LOGIN', refresh);
+        $scope.$on('LOGOUT', refresh);
+
+        function refresh() {
+            vm.userId = AuthService.userId;
+        }
+    }
+
+})();
