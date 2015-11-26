ad6f51d63c72bf53194a8d37f809266da1887bad
iit-federico-bozzini <federico.bozzini@iit.it>
2015-11-26 16:18:05 +0100

    Added a reset.css.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,6 +15,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
+    'styles/reset.css',
     'styles/**/*.css'
 ];
 
