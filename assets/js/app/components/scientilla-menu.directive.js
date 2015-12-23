5963955257d1bb8278a4f8a5c2067ff3f7f07e3a
iit-federico-bozzini <federico.bozzini@iit.it>
2015-12-23 17:46:28 +0100

    Left menu is now dynamically built with js data.

--- assets/js/app/components/scientilla-menu.directive.js
+++ assets/js/app/components/scientilla-menu.directive.js
@@ -3,7 +3,7 @@
 
     angular.module('components')
             .directive('scientillaMenu', scientillaMenu);
-    
+
     function scientillaMenu() {
         return {
             restrict: 'E',
@@ -14,7 +14,7 @@
             }
         };
     }
-    
+
     function scientillaMenuController($scope, AuthService) {
         var vm = this;
 
@@ -24,6 +24,38 @@
         function refresh() {
             vm.isLogged = AuthService.isLogged;
             vm.user = AuthService.user;
+            
+            if (vm.user) {
+                vm.menuItems = [{
+                        title: 'Notifications',
+                        url: '#/users/' + vm.user.id + '/notifications'
+                    },
+                    {
+                        title: 'External References',
+                        url: '#/users/' + vm.user.id + '/external'
+                    },
+                    {
+                        title: 'Personal References',
+                        url: '#/users/' + vm.user.id + '/references'
+                    }
+                ];
+                _.forEach(vm.user.admininstratedGroups, function (g) {
+                    vm.menuItems.push({
+                        title: g.getDisplayName() + ' references',
+                        url: '#/groups/' + g.id + '/references'
+                    });
+                });
+                vm.menuItems = _.union(vm.menuItems, [{
+                        title: 'People',
+                        url: '#/users'
+                    },{
+                        title: 'Groups',
+                        url: '#/groups'
+                    },{
+                        title: 'Profile',
+                        url: '#/users/' + vm.user.id + '/edit'
+                    }]);
+            }
         }
     }
 
