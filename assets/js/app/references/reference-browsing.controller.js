23377770d959de602566b5839a135de584f9458c
iit-federico-bozzini <federico.bozzini@iit.it>
2016-01-26 16:26:19 +0100

    Changed the documents page to use new directives.

--- assets/js/app/references/reference-browsing.controller.js
+++ assets/js/app/references/reference-browsing.controller.js
@@ -4,41 +4,13 @@
             .controller('ReferenceBrowsingController', ReferenceBrowsingController);
 
     ReferenceBrowsingController.$inject = [
-        'ReferencesService',
-        '$route',
         'researchEntity'
     ];
 
-    function ReferenceBrowsingController(ReferencesService, $route, researchEntity) {
+    function ReferenceBrowsingController(researchEntity) {
         var vm = this;
         
         vm.researchEntity = researchEntity;
-        vm.deleteReference = deleteReference;
-        vm.verifyReference = verifyReference;
-        vm.createNewUrl = vm.researchEntity.getNewReferenceUrl();
         vm.editUrl = vm.researchEntity.getProfileUrl();
-        vm.referenceStatusMap = {
-            Public: Scientilla.reference.PUBLIC, 
-            Private: Scientilla.reference.VERIFIED, 
-            Drafts: Scientilla.reference.DRAFT
-        }
-        activate();
-
-        function activate() {
-        }
-
-        function deleteReference(reference) {
-            vm.researchEntity.one('references', reference.id).remove()
-                    .then(function () {
-                        _.remove(vm.researchEntity.references, reference);
-                    });
-        }
-        
-        function verifyReference(reference) {
-            return vm.researchEntity.one('references', reference.id).customPUT({},'verified').then(function(r) {
-                reference.draft = false;
-                reference.status = Scientilla.reference.VERIFIED;
-            });
-        }
     }
 })();
