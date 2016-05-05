eb467efffcc58bb39a644bb10215d2b703e296d5
iit-federico-semprini <federico.semprini@iit.it>
2016-05-05 14:28:04 +0200

    Refactored service

--- assets/js/app/groups/GroupsService.js
+++ assets/js/app/groups/GroupsService.js
@@ -1,81 +1,98 @@
 /* global Scientilla */
 
 (function () {
-    angular.module("groups").factory("GroupsService",
-            ["Restangular", "AuthService", "$http", function (Restangular, AuthService, $http) {
-                    var service = Restangular.service("groups");
 
-                    service.getNewGroup = function () {
-                        return {
-                            name: "",
-                            administrators: [AuthService.user],
-                            memberships: []
-                        };
-                    };
+    angular.module("groups")
+            .factory("GroupsService", GroupService);
 
-                    service.put = function (group) {
-                        return Restangular.copy(group).put();
-                    };
+    GroupService.$inject = ["Restangular", "AuthService", "$http"];
 
-                    service.validateData = function (group) {
-                        //validate user data
-                    };
+    function GroupService(Restangular, AuthService, $http) {
+        var service = Restangular.service("groups");
 
-                    service.getGroupsQuery = function (searchText) {
-                        var qs = {where: {or: [{name: {contains: searchText}}, {description: {contains: searchText}}]}};
-                        var model = 'groups';
-                        return {model: model, qs: qs};
-                    };
+        service.getNewGroup = getNewGroup;
+        service.put = put;
+        service.validateData = validateData;
+        service.getGroupsQuery = getGroupsQuery;
+        service.save = save;
+        service.doSave = doSave;
+        service.getGroups = getGroups;
+        service.getGroupMemebers = getGroupMemebers;
 
-                    service.save = function (group) {
-                        if (group.id)
-                            return group.save();
-                        else
-                            return this.post(group);
-                    };
+        return service;
 
-                    service.doSave = function (group) {
-                        var administrators = group.administrators;
-                        group.administrators = _.map(group.administrators, 'id');
+        function getNewGroup() {
+            return {
+                name: "",
+                administrators: [AuthService.user],
+                memberships: []
+            };
+        }
 
-                        var memberships = _.cloneDeep(group.memberships);
-                        _.forEach(group.memberships, function (m) {
-                            m.user = m.user.id;
+        function put(group) {
+            return Restangular.copy(group).put();
+        }
+
+        function validateData(group) {
+            //validate user data
+        }
+
+        function getGroupsQuery(searchText) {
+            var qs = {where: {or: [{name: {contains: searchText}}, {description: {contains: searchText}}]}};
+            var model = 'groups';
+            return {model: model, qs: qs};
+        }
+
+        function save(group) {
+            if (group.id)
+                return group.save();
+            else
+                return this.post(group);
+        }
+
+        function doSave(group) {
+            var administrators = group.administrators;
+            group.administrators = _.map(group.administrators, 'id');
+
+            var memberships = _.cloneDeep(group.memberships);
+            _.forEach(group.memberships, function (m) {
+                m.user = m.user.id;
+            });
+            return service.save(group).then(function (g) {
+                group.administrators = administrators;
+                group.memberships = memberships;
+                return group;
+            });
+        }
+
+
+        function getGroups(query) {
+            var populate = {populate: ['memberships', 'administrators']};
+
+            var q = _.merge({}, query, populate);
+
+            return this.getList(q);
+        }
+
+        function getGroupMemebers(groupId) {
+            if (!groupId)
+                return;
+
+            return $http.get('/memberships',
+                    {params: {group: groupId, populate: 'user'}})
+                    .then(function (result) {
+                        var memberships = result.data;
+                        _.forEach(memberships, function (m) {
+                            _.defaults(m, Scientilla.membership);
+                            _.defaults(m.user, Scientilla.user);
                         });
-                        return service.save(group).then(function (g) {
-                            group.administrators = administrators;
-                            group.memberships = memberships;
-                            return group;
-                        });
-                    };
+
+                        return memberships;
+                    });
+        }
 
 
-                    service.getGroups = function (query) {
-                        var populate = {populate: ['memberships', 'administrators']};
+        return service;
+    }
 
-                        var q = _.merge({}, query, populate);
-
-                        return this.getList(q);
-                    };
-
-                    service.getGroupMemebers = function (groupId) {
-                        if (!groupId)
-                            return;
-                        
-                        return $http.get('/memberships',
-                                {params: {group: groupId, populate: 'user'}})
-                                .then(function (result) {
-                                    var memberships = result.data;
-                                    _.forEach(memberships, function (m) {
-                                        _.defaults(m, Scientilla.membership);
-                                        _.defaults(m.user, Scientilla.user);
-                                    });
-                                    
-                                    return memberships;
-                                });
-                    };
-
-
-                    return service;
-                }]);
 }());
