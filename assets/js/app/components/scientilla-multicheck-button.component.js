a72919076c102b4b29af16ffc79d0b68e9940150
iit-federico-bozzini <federico.bozzini@iit.it>
2016-05-06 19:08:08 +0200

    Added multicheck component to apply bulk operations.

---
+++ assets/js/app/components/scientilla-multicheck-button.component.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular.module('components')
+            .component('scientillaMulticheckButton', {
+                templateUrl: 'partials/scientillaMulticheckButton.html',
+                controller: scientillaMulticheckButtonController,
+                controllerAs: 'vm',
+                bindings: {
+                    'onClick': '&'
+                },
+                transclude: true,
+                require: {
+                    scientillaMulticheck: '^scientillaMulticheck'
+                }
+            });
+
+
+
+    scientillaMulticheckButtonController.$inject = [
+    ];
+
+    function scientillaMulticheckButtonController() {
+        var vm = this;
+        
+        vm.executeOnClick = executeOnClick;
+        
+        function executeOnClick(){
+            var checkedItems = vm.scientillaMulticheck.getCheckedItems();
+            vm.onClick()(checkedItems);
+        }
+        
+    }
+    
+})();
---
+++ assets/js/app/components/scientilla-multicheck.component.js
@@ -0,0 +1,62 @@
+(function () {
+    'use strict';
+
+    angular.module('components')
+            .component('scientillaMulticheck', {
+                templateUrl: 'partials/scientillaMulticheck.html',
+                controller: scientillaMulticheckController,
+                controllerAs: 'vm',
+                bindings: {
+                    'items': '<'
+                },
+                transclude: {
+                    'item-list': 'itemList',
+                    'buttons': 'buttons'
+                }
+            });
+
+
+
+    scientillaMulticheckController.$inject = [
+    ];
+
+    function scientillaMulticheckController() {
+        var vm = this;
+        vm.registerCheckable = registerCheckable;
+        vm.getCheckedItems = getCheckedItems;
+        vm.selectAll = selectAll;
+        vm.allSelected = false;
+
+        var checkables = [];
+        
+        function registerCheckable(checkable) {
+            checkables.push(checkable);
+        }
+        
+        function getCheckedItems() {
+            return checkables
+                    .filter(function (c) {
+                        return c.isChecked;
+                    })
+                    .map(function (c) {
+                        return c.getItem();
+                    });
+        }
+        
+        function selectAll() {
+            function setAllCheckables(newVal) {
+                checkables.forEach(function(c) {
+                    c.isChecked = newVal;
+                });
+            }
+            var allCheckablesSelected = checkables.every(function(c) {
+                return c.isChecked;
+            });
+            if (vm.allSelected && !allCheckablesSelected)
+                setAllCheckables(true);
+            if (! vm.allSelected && allCheckablesSelected)
+                setAllCheckables(false);
+        }
+    }
+
+})();
