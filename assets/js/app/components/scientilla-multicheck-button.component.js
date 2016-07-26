cbb4a294f32c71c467e06c14303d1075a27ed4fb
iit-federico-semprini <federico.semprini@iit.it>
2016-07-26 16:25:26 +0200

    Added confirm dialog to bulk actions

--- assets/js/app/components/scientilla-multicheck-button.component.js
+++ assets/js/app/components/scientilla-multicheck-button.component.js
@@ -18,20 +18,29 @@
 
 
     scientillaMulticheckButtonController.$inject = [
+        'ModalService'
     ];
 
-    function scientillaMulticheckButtonController() {
+    function scientillaMulticheckButtonController(ModalService) {
         var vm = this;
 
         vm.executeOnClick = executeOnClick;
 
-        this.$onInit = function() {
+        this.$onInit = function () {
             this.scientillaMulticheck.registerButton(vm);
         };
 
         function executeOnClick() {
             var checkedItems = vm.scientillaMulticheck.getCheckedItems();
-            vm.onClick()(checkedItems);
+
+            ModalService
+                    .confirm("", "Apply this action to " + checkedItems.length + " elements?")
+                    .then(function () {
+                        vm.onClick()(checkedItems);
+                    });
+
+
+
         }
 
     }
