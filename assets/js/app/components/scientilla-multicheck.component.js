7694200de0b22459abb9565e8a2aad041d1c94ad
iit-federico-semprini <federico.semprini@iit.it>
2016-07-28 18:12:38 +0200

    Fixed allSelected checked when no checkboxes were checked

--- assets/js/app/components/scientilla-multicheck.component.js
+++ assets/js/app/components/scientilla-multicheck.component.js
@@ -40,6 +40,8 @@
 
         function unregisterCheckable(checkable) {
             _.remove(checkables, checkable);
+            
+            vm.allSelected &= areElementsSelected();
         }
 
         function registerButton(button) {
