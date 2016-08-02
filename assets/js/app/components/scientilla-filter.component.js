47ffda6b07a25ded8d378dced35e682364a45da6
iit-federico-bozzini <federico.bozzini@iit.it>
2016-08-02 16:01:17 +0200

    Refactored the filter component (eliminated search component).

--- assets/js/app/components/scientilla-filter.component.js
+++ assets/js/app/components/scientilla-filter.component.js
@@ -23,10 +23,12 @@
 
     function scientillaFilter(pageSize) {
         var vm = this;
-        
+
         vm.onSearch = onSearch;
         vm.onPageChange = onPageChange;
         vm.onStatus = onStatus;
+        vm.search = search;
+        vm.reset = reset;
 
         vm.itemsPerPage = pageSize;
         vm.currentPage = 1;
@@ -36,13 +38,9 @@
         vm.STATUS_WAITING = 0;
         vm.STATUS_LOADING = 1;
 
-        vm.status = vm.STATUS_WAITING;
-
-        if (vm.emptyListMessage === undefined)
-            vm.emptyListMessage = "No results found";
-
         var searchQuery = {};
 
+        activate();
 
         function onSearch(searchWhere) {
             vm.currentPage = 1;
@@ -50,6 +48,7 @@
 
             refreshList();
         }
+
         function onPageChange() {
             refreshList();
         }
@@ -59,7 +58,68 @@
             return vm.status === status;
         }
 
+        function search() {
+            var where = {};
+
+            _.forEach(this.searchValues,
+                    function (value, key) {
+
+                        var struct = vm.searchFormStructure[key];
+
+                        if (struct.inputType === 'select' && vm.searchValues[key] === "?")
+                            return;
+
+                        var whereAdd = {};
+                        if (!struct.matchRule) {
+                            whereAdd[struct.matchColumn] = value;
+                        }
+                        else if (struct.matchRule === 'is null') {
+                            if (!value)
+                                whereAdd[struct.matchColumn] = null;
+                        }
+                        else {
+                            whereAdd[struct.matchColumn] = {};
+                            whereAdd[struct.matchColumn][struct.matchRule] = value;
+                        }
+                        where = _.merge(where, whereAdd);
+                    });
+            vm.onSearch(where);
+        }
+
+        function reset() {
+            _.forEach(this.searchValues,
+                    function (value, key) {
+
+                        var struct = vm.searchFormStructure[key];
+                        if (struct.inputType === 'select') {
+                            vm.searchValues[key] = "?";
+                        }
+                        else {
+
+                            if (struct.defaultValue)
+                                vm.searchValues[key] = struct.defaultValue;
+                            else
+                                vm.searchValues[key] = '';
+                        }
+                    });
+            vm.search();
+        }
+
         // private
+
+        function activate() {
+            vm.status = vm.STATUS_WAITING;
+
+            if (_.isUndefined(vm.emptyListMessage))
+                vm.emptyListMessage = "No results found";
+
+            if (_.isUndefined(vm.filterLabel))
+                vm.filterLabel = "Filter";
+
+            vm.searchValues = getSearchValues();
+
+            vm.search();
+        }
 
         function refreshList() {
             setStatus(vm.STATUS_LOADING);
@@ -78,7 +138,6 @@
                 vm.message = list.length === 0 ? vm.emptyListMessage : "";
                 setStatus(vm.STATUS_WAITING);
             });
-
         }
 
         function getQuery() {
@@ -87,7 +146,6 @@
                 limit: pageSize + 1
             };
 
-
             return _.merge({}, paginationQuery, searchQuery);
         }
 
@@ -95,7 +153,17 @@
             return vm.status = status;
         }
 
+        function getSearchValues() {
+            var searchValues = {};
+
+            _.forEach(vm.searchFormStructure, function (value, key) {
+                if (!_.isUndefined(value.defaultValue))
+                    searchValues[key] = value.defaultValue;
+            });
+
+            return searchValues;
+
+        }
+
     }
-
-
 })();
