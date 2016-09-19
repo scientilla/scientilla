78e31b96f5012b1342936f336759342af55211a1
iit-federico-bozzini <federico.bozzini@iit.it>
2016-09-19 11:31:39 +0200

    Added test of verification two equal documents.

--- test/api/draftVerification.test.js
+++ test/api/draftVerification.test.js
@@ -9,11 +9,15 @@ describe('Draft Verification', function () {
     after(test.cleanDb);
 
     var url = test.getUrl();
-    var userData = test.getUsers()[0];
+    var user1Data = test.getUsers()[0];
+    var user2Data = test.getUsers()[1];
     var documentData = test.getDocuments()[0];
     var incompleteDocumentData = test.getDocuments()[1];
-    var user;
-    var draft;
+    var user1;
+    var user2;
+    var user1Draft1;
+    var user1Draft2;
+    var user2Draft1;
 
     it('there should be no verified documents for a new user', function () {
         return request(url)
@@ -22,43 +26,43 @@ describe('Draft Verification', function () {
                 .then(function (res) {
                     return request(url)
                             .post('/auths/register')
-                            .send(userData)
+                            .send(user1Data)
                             .expect(200);
                 })
                 .then(function (res) {
-                    user = res.body;
+                    user1 = res.body;
                     return res;
                 })
                 .then(function (res) {
                     return request(url)
-                            .get('/users/' + user.id + '/privateReferences')
+                            .get('/users/' + user1.id + '/privateReferences')
                             .expect(200, []);
                 });
     });
 
     it('verifying a complete draft should be possible', function () {
         return request(url)
-                .post('/users/' + user.id + '/drafts')
+                .post('/users/' + user1.id + '/drafts')
                 .send(documentData)
                 .expect(200)
                 .then(function (res) {
-                    draft = res.body;
+                    user1Draft1 = res.body;
                     return res;
                 })
                 .then(function (res) {
                     return request(url)
-                            .put('/users/' + user.id + '/drafts/' + draft.id + '/verified')
+                            .put('/users/' + user1.id + '/drafts/' + user1Draft1.id + '/verified')
                             .send(documentData)
                             .expect(200);
                 })
                 .then(function (res) {
                     return request(url)
-                            .get('/users/' + user.id + '/drafts')
+                            .get('/users/' + user1.id + '/drafts')
                             .expect(200, []);
                 })
                 .then(function (res) {
                     return request(url)
-                            .get('/users/' + user.id + '/privateReferences?populate=privateCoauthors')
+                            .get('/users/' + user1.id + '/privateReferences?populate=privateCoauthors')
                             .expect(function (res) {
                                 res.status.should.equal(200);
                                 res.body.should.have.length(1);
@@ -67,30 +71,30 @@ describe('Draft Verification', function () {
                                 document.draft.should.be.false;
                                 should(document.draftCreator).be.null;
                                 document.privateCoauthors.should.have.length(1);
-                                document.privateCoauthors[0].username.should.equal(user.username);
+                                document.privateCoauthors[0].username.should.equal(user1.username);
                             });
                 });
     });
 
     it('verifying a complete draft twice should give an error', function () {
         return request(url)
-                .put('/users/' + user.id + '/drafts/' + draft.id + '/verified')
+                .put('/users/' + user1.id + '/drafts/' + user1Draft1.id + '/verified')
                 .send(documentData)
                 .expect(400);
     });
 
     it('verifying a non complete draft should not be possible', function () {
         return request(url)
-                .post('/users/' + user.id + '/drafts')
+                .post('/users/' + user1.id + '/drafts')
                 .send(incompleteDocumentData)
                 .expect(200)
                 .then(function (res) {
-                    draft = res.body;
+                    user1Draft2 = res.body;
                     return res;
                 })
                 .then(function (res) {
                     return request(url)
-                            .put('/users/' + user.id + '/drafts/' + draft.id + '/verified')
+                            .put('/users/' + user1.id + '/drafts/' + user1Draft2.id + '/verified')
                             .send(documentData)
                             .expect(function (res) {
                                 res.status.should.equal(200);
@@ -99,7 +103,7 @@ describe('Draft Verification', function () {
                 })
                 .then(function (res) {
                     return request(url)
-                            .get('/users/' + user.id + '/drafts')
+                            .get('/users/' + user1.id + '/drafts')
                             .expect(function (res) {
                                 res.status.should.equal(200);
                                 res.body.should.have.length(1);
@@ -107,7 +111,7 @@ describe('Draft Verification', function () {
                 })
                 .then(function (res) {
                     return request(url)
-                            .get('/users/' + user.id + '/privateReferences')
+                            .get('/users/' + user1.id + '/privateReferences')
                             .expect(function (res) {
                                 res.status.should.equal(200);
                                 res.body.should.have.length(1);
@@ -117,9 +121,52 @@ describe('Draft Verification', function () {
 
     it('verifying a nonexsting document should give an error', function () {
         return request(url)
-                .put('/users/' + user.id + '/drafts/' + 10000 + '/verified')
+                .put('/users/' + user1.id + '/drafts/' + 10000 + '/verified')
                 .send(documentData)
                 .expect(400);
+    });
+
+    it('verifying two identical documents should merge them', function () {
+        return request(url)
+                .post('/auths/register')
+                .send(user2Data)
+                .expect(200)
+                .then(function (res) {
+                    user2 = res.body;
+                    return res;
+                })
+                .then(function (res) {
+                    return request(url)
+                            .post('/users/' + user2.id + '/drafts')
+                            .send(documentData)
+                            .expect(200);
+                })
+                .then(function (res) {
+                    user2Draft1 = res.body;
+                    return res;
+                })
+                .then(function (res) {
+                    return request(url)
+                            .put('/users/' + user2.id + '/drafts/' + user2Draft1.id + '/verified')
+                            .send(documentData)
+                            .expect(200);
+                })
+                .then(function (res) {
+                    return request(url)
+                            .get('/users/' + user2.id + '/privateReferences?populate=privateCoauthors')
+                            .expect(function (res) {
+                                res.status.should.equal(200);
+                                res.body.should.have.length(1);
+                                var document = res.body[0];
+                                document.id.should.equal(user1Draft1.id);
+                                document.title.should.equal(documentData.title);
+                                document.draft.should.be.false;
+                                should(document.draftCreator).be.null;
+                                document.privateCoauthors.should.have.length(2);
+                                document.privateCoauthors[0].username.should.equal(user1.username);
+                                document.privateCoauthors[1].username.should.equal(user2.username);
+                            });
+                });
     });
 
 });
--- test/helper.js
+++ test/helper.js
@@ -14,7 +14,7 @@ module.exports = {
                 .then(_ => done())
                 .catch(done);
     },
-    createModel: function(Model, data) {
+    createModel: function (Model, data) {
         return _.defaults(data, Model.attributes);
     },
     finalCheck: function (done) {
@@ -37,15 +37,20 @@ module.exports = {
 };
 
 var users = [{
-        username: 'federico.bozzini@gmail.com',
+        username: 'federico.bozzini@example.com',
         password: 'userpass',
         name: 'Federico',
         lastName: 'Bozzini'
+    }, {
+        username: 'federico.semprini@example.com',
+        password: 'userpass',
+        name: 'Federico',
+        lastName: 'Semprini'
     }];
 
 var documents = [{
         title: "A Bayesian approach towards affordance learning in artificial agents",
-        authors: "Stramandinoli F., Tikhanoff V., Pattacini U., Nori F. and Bozzini F.",
+        authors: "Semprini F., Tikhanoff V., Pattacini U., Nori F. and Bozzini F.",
         year: "2015",
         journal: null,
         issue: null,
@@ -64,7 +69,7 @@ var documents = [{
         scopusId: "84962170621",
         wosId: null,
         abstract: "Inspired by recent advances proposed in the ecological psychology community, many developmental robotics studies have started to investigate the modeling and learning of affordances in humanoid robots. In this paper we leverage a probabilistic graphical model in place of the Least Square Support Vector Machine (LSSVM) used in a previous experiment, for testing the Bayesian approach towards affordance learning in the iCub robot. We present two experiments related to the learning of the effect consequent from the tapping of objects from several directions and to the pulling of out-of-reach objects by choosing the appropriate tool. The proposed probabilistic graphical model w.r.t the LSSVM not only identifies a regression function for the prediction of the effects of actions but it provides information on the reliability of the predicted values as well."
-    },{
+    }, {
         title: "A Bayesian approach towards affordance learning in artificial agents",
         authors: "Stramandinoli F., Tikhanoff V., Pattacini U., Nori F. and Bozzini F.",
         year: "2015",
