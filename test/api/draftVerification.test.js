a41c0194964c35bc5c50efae1bf1f61ba33aec19
iit-federico-bozzini <federico.bozzini@iit.it>
2016-09-30 17:58:06 +0200

    Improved tests.

--- test/api/draftVerification.test.js
+++ test/api/draftVerification.test.js
@@ -20,131 +20,144 @@ describe('Draft Verification', function () {
 
     it('there should be no verified documents for a new user', function () {
         return test
-                .registerUser(user1Data)
-                .then(function (res) {
-                    user1 = res.body;
-                    return res;
-                })
-                .then(function (res) {
-                    test
-                            .getDocuments(user1)
-                            .expect(200, []);
-                });
+            .registerUser(user1Data)
+            .then(function (res) {
+                user1 = res.body;
+                return res;
+            })
+            .then(function (res) {
+                test
+                    .getDocuments(user1)
+                    .expect(200, []);
+            });
     });
 
     it('verifying a complete draft should be possible', function () {
         return test
-                .createDraft(user1, documentData)
-                .then(function (res) {
-                    user1Draft1 = res.body;
-                    return res;
-                })
-                .then(function (res) {
-                    return test.verifyDraft(user1, user1Draft1);
-                })
-                .then(function (res) {
-                    return test
-                            .getDrafts(user1)
-                            .expect(200, []);
-                })
-                .then(function (res) {
-                    return test
-                            .getDocumentsWithAuthors(user1)
-                            .expect(function (res) {
-                                res.status.should.equal(200);
-                                res.body.should.have.length(1);
-                                var document = res.body[0];
-                                document.title.should.equal(documentData.title);
-                                document.draft.should.be.false;
-                                should(document.draftCreator).be.null;
-                                document.privateCoauthors.should.have.length(1);
-                                document.privateCoauthors[0].username.should.equal(user1.username);
-                            });
-                });
+            .createDraft(user1, documentData)
+            .then(function (res) {
+                user1Draft1 = res.body;
+                return res;
+            })
+            .then(function (res) {
+                return test
+                    .verifyDraft(user1, user1Draft1)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        var document = res.body;
+                        document.title.should.equal(documentData.title);
+                        document.draft.should.be.false;
+                        should(document.draftCreator).be.null;
+                    });
+            })
+            .then(function (res) {
+                return test
+                    .getDrafts(user1)
+                    .expect(200, []);
+            })
+            .then(function (res) {
+                return test
+                    .getDocumentsWithAuthors(user1)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        res.body.should.have.length(1);
+                        var document = res.body[0];
+                        document.title.should.equal(documentData.title);
+                        document.draft.should.be.false;
+                        should(document.draftCreator).be.null;
+                        document.privateCoauthors.should.have.length(1);
+                        document.privateCoauthors[0].username.should.equal(user1.username);
+                    });
+            });
     });
 
     it('verifying a complete draft twice should give an error', function () {
         return test
-                .verifyDraft(user1, user1Draft1)
-                .expect(400);
+            .verifyDraft(user1, user1Draft1)
+            .expect(400);
     });
 
     it('verifying a non complete draft should not be possible', function () {
         return test
-                .createDraft(user1, incompleteDocumentData)
-                .then(function (res) {
-                    user1Draft2 = res.body;
-                    return res;
-                })
-                .then(function (res) {
-                    return test
-                            .verifyDraft(user1, user1Draft2)
-                            .expect(function (res) {
-                                res.status.should.equal(200);
-                                res.body.draft.should.be.true;
-                            });
-                })
-                .then(function (res) {
-                    return test
-                            .getDrafts(user1)
-                            .expect(function (res) {
-                                res.status.should.equal(200);
-                                res.body.should.have.length(1);
-                            });
-                })
-                .then(function (res) {
-                    return test
-                            .getDocuments(user1)
-                            .expect(function (res) {
-                                res.status.should.equal(200);
-                                res.body.should.have.length(1);
-                            });
-                });
+            .createDraft(user1, incompleteDocumentData)
+            .then(function (res) {
+                user1Draft2 = res.body;
+                return res;
+            })
+            .then(function (res) {
+                return test
+                    .verifyDraft(user1, user1Draft2)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        res.body.draft.should.be.true;
+                    });
+            })
+            .then(function (res) {
+                return test
+                    .getDrafts(user1)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        res.body.should.have.length(1);
+                    });
+            })
+            .then(function (res) {
+                return test
+                    .getDocuments(user1)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        res.body.should.have.length(1);
+                    });
+            });
     });
 
     it('verifying a nonexsting document should give an error', function () {
         return test
-                .verifyDraft(user1, nonExistentDocument)
-                .expect(400);
+            .verifyDraft(user1, nonExistentDocument)
+            .expect(400);
     });
 
     it('verifying two identical documents should merge them', function () {
         return test.
-                registerUser(user2Data)
-                .then(function (res) {
-                    user2 = res.body;
-                    return res;
-                })
-                .then(function (res) {
-                    return test
-                            .createDraft(user2, documentData)
-                            .expect(200);
-                })
-                .then(function (res) {
-                    user2Draft1 = res.body;
-                    return res;
-                })
-                .then(function (res) {
-                    return test
-                            .verifyDraft(user2, user2Draft1)
-                            .expect(200);
-                })
-                .then(function (res) {
-                    return test
-                            .getDocumentsWithAuthors(user2)
-                            .expect(function (res) {
-                                res.status.should.equal(200);
-                                res.body.should.have.length(1);
-                                var document = res.body[0];
-                                document.id.should.equal(user1Draft1.id);
-                                document.title.should.equal(documentData.title);
-                                document.draft.should.be.false;
-                                should(document.draftCreator).be.null;
-                                document.privateCoauthors.should.have.length(2);
-                                document.privateCoauthors[0].username.should.equal(user1.username);
-                                document.privateCoauthors[1].username.should.equal(user2.username);
-                            });
+        registerUser(user2Data)
+            .then(function (res) {
+                user2 = res.body;
+                return res;
+            })
+            .then(function (res) {
+                return test
+                    .createDraft(user2, documentData)
+                    .expect(200);
+            })
+            .then(function (res) {
+                user2Draft1 = res.body;
+                return res;
+            })
+            .then(function (res) {
+                return test
+                    .verifyDraft(user2, user2Draft1)
+                    .expect(function (res) {
+                    res.status.should.equal(200);
+                    var document = res.body;
+                    document.title.should.equal(documentData.title);
+                    document.draft.should.be.false;
+                    should(document.draftCreator).be.null;
                 });
+            })
+            .then(function (res) {
+                return test
+                    .getDocumentsWithAuthors(user2)
+                    .expect(function (res) {
+                        res.status.should.equal(200);
+                        res.body.should.have.length(1);
+                        var document = res.body[0];
+                        document.id.should.equal(user1Draft1.id);
+                        document.title.should.equal(documentData.title);
+                        document.draft.should.be.false;
+                        should(document.draftCreator).be.null;
+                        document.privateCoauthors[0].username.should.equal(user1.username);
+                        document.privateCoauthors[1].username.should.equal(user2.username);
+                    });
+            });
     });
 
 });
