1a0761ed07219775dbe4690fbb7ab8c90f4144ed
iit-federico-bozzini <federico.bozzini@iit.it>
2016-10-13 16:06:54 +0200

    Substituted machinepack-sql-builder with waterline-sql-builder.

--- api/services/SqlService.js
+++ api/services/SqlService.js
@@ -5,48 +5,42 @@
 
 var _ = require('lodash');
 var Promise = require("bluebird");
-var Pack = require('machinepack-sql-builder');
+var GeneratorFn = require('waterline-sql-builder');
 
 module.exports = {
     dialect: 'postgresql',
     generateFromJson: function (query) {
-
         var dialect = this.dialect;
+        var generator = GeneratorFn({ dialect: dialect });
 
-        var generateSql = function (q) {
-            return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
+            try {
+                var result = generator.generate(query);
+                var sql = result.sql;
 
-                Pack.generateSql({
-                    dialect: dialect,
-                    query: q
-                }).exec({
-                    success: resolve,
-                    error: reject
+                _.forEach(result.bindings, function (binding, i) {
+                    var key = '\\$' + (i + 1);
+                    var regex = new RegExp('([\\s(]){1}' + key + '([\\s,)]|$){1}');
+
+                    // TODO escape binding
+                    var value;
+                    var uppercaseBinding = _.upperCase(binding);
+                    if (_.includes(['FALSE', 'TRUE'], uppercaseBinding))
+                        value = uppercaseBinding === "TRUE";
+                    else
+                        value = parseInt(binding, 10) || "'" + binding + "'";
+
+                    sql = _.replace(sql, regex, '$1' + value + '$2');
                 });
-            });
-        };
+                resolve(sql);
+            } catch(e) {
+                reject(e);
+            }
+        });
 
 
         return generateSql(query)
                 .then(function (result) {
-                    var sql = result.sql;
-
-                    _.forEach(result.bindings, function (binding, i) {
-                        var key = '\\$' + (i + 1);
-                        var regex = new RegExp('([\\s(]){1}' + key + '([\\s,)]|$){1}');
-
-                        // TODO escape binding
-                        var value;
-                        var uppercaseBinding = _.upperCase(binding);
-                        if (_.includes(['FALSE', 'TRUE'], uppercaseBinding))
-                            value = uppercaseBinding === "TRUE";
-                        else
-                            value = parseInt(binding, 10) || "'" + binding + "'";
-
-                        sql = _.replace(sql, regex, '$1' + value + '$2');
-                    });
-
-                    return sql;
                 });
     },
     query: function (sql) {
@@ -56,8 +50,9 @@ module.exports = {
                     return result.rows;
                 })
                 .catch(function (err) {
-                    console.log(sql);
-                    console.log(err);
+                    sails.log.debug('The following query generated an error');
+                    sails.log.debug(sql);
+                    sails.log.debug(err);
                 });
     }
 };
