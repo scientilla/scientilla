1e99cdac246000139d1b86bdde1d89334663d4e8
iit-federico-bozzini <federico.bozzini@iit.it>
2016-10-24 09:47:50 +0200

    Added missing files.

---
+++ api/controllers/AffiliationController.js
@@ -0,0 +1,11 @@
+/**
+ * AffiliationController
+ *
+ * @description :: Server-side logic for managing Affiliations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Institute.js
@@ -0,0 +1,26 @@
+/**
+ * Institute.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        name: 'STRING',
+        country: 'STRING',
+        city: 'STRING',
+        shortname: 'STRING',
+        scopusId: 'STRING',
+        group: {
+            model: 'group'
+        },
+        authorships: {
+            collection: 'authorship',
+            via: 'affiliations',
+            through: 'affiliation'
+        }
+    }
+};
+
