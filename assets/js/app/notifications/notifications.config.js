0516a21b3f7fbd6f7c5a11be6d503da74dad46a5
iit-federico-bozzini <federico.bozzini@iit.it>
2016-10-26 17:40:48 +0200

    Removed unused code.

--- assets/js/app/notifications/notifications.config.js
+++ assets/js/app/notifications/notifications.config.js
@@ -10,27 +10,8 @@
                 .when("/users/:id/notifications", {
                     templateUrl: "partials/notification-browsing.html",
                     controller: "NotificationBrowsingController",
-                    controllerAs: 'vm',
-                    resolve: {
-                        NotificationsService: NotificationsService,
-                        user: getCurrentUser
-                    },
+                    controllerAs: 'vm'
                 });
     }
 
-    NotificationsService.$inject = ['NotificationsServiceFactory', 'AuthService', '$route'];
-    
-    function NotificationsService(NotificationsServiceFactory, AuthService, $route) {
-        return NotificationsServiceFactory($route.current.params.id);
-    }
-    
-    
-    //sTODO: move this function to the UsersService
-    getCurrentUser.$inject = ['UsersService', '$route'];
-
-    function getCurrentUser(UsersService, $route) {
-        var userId = $route.current.params.id;
-        return UsersService.one(userId).get({populate: ['collaborations', 'aliases']});
-    }
-    
 })();
