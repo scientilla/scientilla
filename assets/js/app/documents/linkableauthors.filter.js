877af27cf794b18160c9e4fe3a3dd3387fc12c7e
iit-federico-bozzini <federico.bozzini@iit.it>
2016-10-26 13:58:13 +0200

    Affiliations are now shown on the author string of a document.

--- assets/js/app/documents/linkableauthors.filter.js
+++ assets/js/app/documents/linkableauthors.filter.js
@@ -4,27 +4,33 @@
 
     function linkableAuthors() {
 
-        function getLinkableAuthors(reference) {
-            if (!reference.authorsStr) return "";
-            if (!_.isFunction(reference.getAuthors))
-                return reference.authorsStr;
+        function getLinkableAuthors(document) {
+            var mainGroupId = 1;
+            if (!document.authorsStr) return "";
+            if (!_.isFunction(document.getAuthors))
+                return document.authorsStr;
 
-            var authors = reference.getAuthors();
-            var possibleMatches = reference.getAllCoauthors();
-            var linkedAuthorsStr = reference.authorsStr;
-            authors.forEach(function (author) {
+            var authors = document.getAuthors();
+            var possibleMatches = document.getAllCoauthors();
+            var linkedAuthors = authors.map(function (author) {
                 var ucAuthor = author.toUpperCase();
                 var matchingUser = _.find(possibleMatches, function (c) {
                     var aliases = c.getUcAliases();
                     return _.includes(aliases, ucAuthor);
                     
                 });
-                if (matchingUser) {
-                    //TODO: add offset to avoid substituting already substituted users
-                    linkedAuthorsStr = linkedAuthorsStr.replace(author, '<a href="#/users/'+ matchingUser.id+'">' + author + '</a>');
-                }
+                if (!matchingUser)
+                    return author
+                 return '<a href="#/users/'+ matchingUser.id+'">' + author + '</a>';
             });
-            return linkedAuthorsStr;
+            var authorsWithMainGroup = _.map(linkedAuthors, function (author, i) {
+                var authorship = _.find(document.authorships, a => a.position === i)
+                if (!authorship || !authorship.affiliations.includes(mainGroupId))
+                    return author;
+                return author + '<a href="#/groups/1"><sup class="superscript">IIT</sup></a>';
+            });
+
+            return authorsWithMainGroup.join(', ');
         }
 
         return getLinkableAuthors;
