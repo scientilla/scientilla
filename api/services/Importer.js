4776167be6a79bd51b81d04d9bc748e1c73e1a5b
iit-federico-bozzini <federico.bozzini@iit.it>
2016-12-14 13:55:47 +0100

    Moved method to proper class.

--- api/services/Importer.js
+++ api/services/Importer.js
@@ -2,7 +2,9 @@
 
 "use strict";
 
+const xlsx = require('xlsx');
 const _ = require('lodash');
+const fs = require('fs');
 
 const startingYear = sails.config.scientilla.mainInstituteImport.startingYear;
 
@@ -17,7 +19,7 @@ module.exports = {
                 institute = i;
                 return Importer.importScopusDocuments(institute)
             })
-            .then(unused=>true);
+            .then(unused => true);
     },
     importScopusDocuments: function (institute) {
 
@@ -64,7 +66,7 @@ module.exports = {
                 .then(result => {
                     const documents = result.items;
                     Group.createDrafts(Group, institute.id, documents)
-                        .then(docs => Group.verifyDrafts(Group, institute.id, docs.map(d=>d.id)));
+                        .then(docs => Group.verifyDrafts(Group, institute.id, docs.map(d => d.id)));
 
                     if (result.count <= (query.limit + query.skip))
                         return;
@@ -76,6 +78,101 @@ module.exports = {
                 })
         }
 
+    },
 
+    readSourcesFromExcel: function () {
+
+        function readWorksheet(worksheet, mappingsTable, mapFn = _.identity, filterFn = _.stubTrue) {
+            function readSourceRow(r) {
+                const sourceData = {};
+                _.forEach(mappingsTable, (col, field) => {
+                    const cell = col + r;
+                    if (!_.isUndefined(worksheet[cell]))
+                        sourceData[field] = worksheet[cell]['v'];
+                });
+                return sourceData;
+            }
+
+            const sources = [];
+            for (let r = 2; ; r++) {
+                const sourceData = readSourceRow(r);
+                if (_.isEmpty(sourceData))
+                    break;
+                const mappedSourceData = mapFn(sourceData);
+                if (filterFn(mappedSourceData))
+                    sources.push(mappedSourceData);
+            }
+            return sources;
+        }
+
+        const scopusSourcesFileName = 'config/init/scopus_sources.xlsx';
+        let journalsAndBookSeries = [], newConferences = [], oldConferences = [];
+        if (fs.existsSync(scopusSourcesFileName)) {
+            const workbook = xlsx.readFile(scopusSourcesFileName);
+            const sheetNameList = workbook.SheetNames;
+
+            const journalWorksheet = workbook.Sheets[sheetNameList[0]];
+            const journalMappingsTable = {
+                "title": 'B',
+                "scopusId": 'A',
+                "issn": 'C',
+                "eissn": 'D',
+                "type": 'T',
+                "publisher": 'Z'
+            };
+
+            const mapJournal = (s) => {
+                if (s.type == 'Book Series')
+                    s.type = 'bookseries';
+                return s;
+            };
+
+            const filterJournals = (s) => s.type != 'Trade Journal';
+
+            journalsAndBookSeries = readWorksheet(journalWorksheet, journalMappingsTable, mapJournal, filterJournals);
+
+            const newConferencesWorksheet = workbook.Sheets[sheetNameList[1]];
+            const newConferencesMappingsTable = {
+                "title": 'B',
+                "scopusId": 'A',
+                "issn": 'D'
+            };
+            const mapConference = s => {
+                s.type = 'conference';
+                return s;
+            };
+            newConferences = readWorksheet(newConferencesWorksheet, newConferencesMappingsTable, mapConference);
+
+            const oldConferencesWorksheet = workbook.Sheets[sheetNameList[2]];
+            const oldConferencesMappingsTable = newConferencesMappingsTable;
+            oldConferences = readWorksheet(oldConferencesWorksheet, oldConferencesMappingsTable, mapConference);
+        }
+
+
+        let books = [];
+        const scopusSourcesFileName2 = 'config/init/scopus_book_sources.xlsx';
+        if (fs.existsSync(scopusSourcesFileName2)) {
+            const workbook2 = xlsx.readFile(scopusSourcesFileName2);
+            const sheetNameList2 = workbook2.SheetNames;
+
+            const bookWorksheet = workbook2.Sheets[sheetNameList2[0]];
+            const bookMappingsTable = {
+                "title": 'A',
+                "isbn": 'C',
+                "publisher": 'D',
+                "year": 'E'
+            };
+
+            const mapBook = (s) => {
+                s.type = 'book';
+                return s;
+            };
+
+            books = readWorksheet(bookWorksheet, bookMappingsTable, mapBook);
+        }
+
+        const allSourceData = _.union(journalsAndBookSeries, newConferences, oldConferences, books);
+
+        return allSourceData;
     }
 };
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,7 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 'use strict';
-const xlsx = require('xlsx');
 const _ = require('lodash');
-const fs = require('fs');
 
 module.exports.bootstrap = function (cb) {
     // It's very important to trigger this callback method when you are finished
@@ -63,105 +61,9 @@ module.exports.bootstrap = function (cb) {
             .then(sourcesNum => {
                 if (sourcesNum)
                     return;
-                const sources = readFromExcel();
+                const sources = Importer.readSourcesFromExcel();
                 sails.log.info('Inserting ' + sources.length + ' new sources');
                 return Source.create(sources);
             });
-
-        function readFromExcel() {
-
-            function readWorksheet(worksheet, mappingsTable, mapFn = _.identity, filterFn = _.stubTrue) {
-                function readSourceRow(r) {
-                    const sourceData = {};
-                    _.forEach(mappingsTable, (col, field) => {
-                        const cell = col + r;
-                        if (!_.isUndefined(worksheet[cell]))
-                            sourceData[field] = worksheet[cell]['v'];
-                    });
-                    return sourceData;
-                }
-
-                const sources = [];
-                for (let r = 2; ; r++) {
-                    const sourceData = readSourceRow(r);
-                    if (_.isEmpty(sourceData))
-                        break;
-                    const mappedSourceData = mapFn(sourceData);
-                    if (filterFn(mappedSourceData))
-                        sources.push(mappedSourceData);
-                }
-                return sources;
-            }
-
-            const scopusSourcesFileName = 'config/init/scopus_sources.xlsx';
-            let journalsAndBookSeries = [], newConferences = [], oldConferences = [];
-            if (fs.existsSync(scopusSourcesFileName)) {
-                const workbook = xlsx.readFile(scopusSourcesFileName);
-                const sheetNameList = workbook.SheetNames;
-
-                const journalWorksheet = workbook.Sheets[sheetNameList[0]];
-                const journalMappingsTable = {
-                    "title": 'B',
-                    "scopusId": 'A',
-                    "issn": 'C',
-                    "eissn": 'D',
-                    "type": 'T',
-                    "publisher": 'Z'
-                };
-
-                const mapJournal = (s) => {
-                    if (s.type == 'Book Series')
-                        s.type = 'bookseries';
-                    return s;
-                };
-
-                const filterJournals = (s) => s.type != 'Trade Journal';
-
-                journalsAndBookSeries = readWorksheet(journalWorksheet, journalMappingsTable, mapJournal, filterJournals);
-
-                const newConferencesWorksheet = workbook.Sheets[sheetNameList[1]];
-                const newConferencesMappingsTable = {
-                    "title": 'B',
-                    "scopusId": 'A',
-                    "issn": 'D'
-                };
-                const mapConference = s => {
-                    s.type = 'conference';
-                    return s;
-                };
-                newConferences = readWorksheet(newConferencesWorksheet, newConferencesMappingsTable, mapConference);
-
-                const oldConferencesWorksheet = workbook.Sheets[sheetNameList[2]];
-                const oldConferencesMappingsTable = newConferencesMappingsTable;
-                oldConferences = readWorksheet(oldConferencesWorksheet, oldConferencesMappingsTable, mapConference);
-            }
-
-
-            let books = [];
-            const scopusSourcesFileName2 = 'config/init/scopus_book_sources.xlsx';
-            if (fs.existsSync(scopusSourcesFileName2)) {
-                const workbook2 = xlsx.readFile(scopusSourcesFileName2);
-                const sheetNameList2 = workbook2.SheetNames;
-
-                const bookWorksheet = workbook2.Sheets[sheetNameList2[0]];
-                const bookMappingsTable = {
-                    "title": 'A',
-                    "isbn": 'C',
-                    "publisher": 'D',
-                    "year": 'E'
-                };
-
-                const mapBook = (s) => {
-                    s.type = 'book';
-                    return s;
-                };
-
-                books = readWorksheet(bookWorksheet, bookMappingsTable, mapBook);
-            }
-
-            const allSourceData = _.union(journalsAndBookSeries, newConferences, oldConferences, books);
-
-            return allSourceData;
-        }
     }
 };
