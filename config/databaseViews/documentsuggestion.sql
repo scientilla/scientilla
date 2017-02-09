526485a87d8d9025730b5a4b15638ace9b1cdd4f
iit-federico-bozzini <federico.bozzini@iit.it>
2017-02-09 16:16:24 +0100

    Only verified documents are suggested.

--- config/databaseViews/documentsuggestion.sql
+++ config/databaseViews/documentsuggestion.sql
@@ -1,20 +1,28 @@
 CREATE OR REPLACE VIEW documentsuggestion AS
   SELECT
-    "document"."id" AS "document",
-    "user"."id"      AS "researchEntity"
+    d."id"      AS "document",
+    "user"."id" AS "researchEntity"
   FROM "user"
-    JOIN "document"
-      ON "document"."authorsStr" ILIKE '%' :: TEXT || "user"."surname" || '%' :: TEXT
-  WHERE "document"."draft" = FALSE
-        AND
-        "document"."id" NOT IN (
-          SELECT "authorship"."document"
-          FROM "authorship"
-          WHERE "authorship"."researchEntity" = "user"."id"
-        )
-        AND
-        "document"."id" NOT IN (
-          SELECT "discarded"."document"
-          FROM "discarded"
-          WHERE "discarded"."researchEntity" = "user"."id"
-        )
+    JOIN "document" d
+      ON d."authorsStr" ILIKE '%' :: TEXT || "user"."surname" || '%' :: TEXT
+    JOIN (SELECT a.document
+          FROM "authorship" a
+          WHERE a."researchEntity" IS NOT NULL
+          UNION
+          SELECT a.document
+          FROM "authorshipgroup" a
+          WHERE a."researchEntity" IS NOT NULL
+         ) AS verified
+      ON d.id = verified.document
+  WHERE
+    d."id" NOT IN (
+      SELECT "authorship"."document"
+      FROM "authorship"
+      WHERE "authorship"."researchEntity" = "user"."id"
+    )
+    AND
+    d."id" NOT IN (
+      SELECT "discarded"."document"
+      FROM "discarded"
+      WHERE "discarded"."researchEntity" = "user"."id"
+    )
