58e21474862852243762eda0c2ce61efb52f2918
iit-federico-bozzini <federico.bozzini@iit.it>
2017-02-09 16:16:44 +0100

    Fixed formatting.

--- config/databaseViews/documentsuggestiongroup.sql
+++ config/databaseViews/documentsuggestiongroup.sql
@@ -1,16 +1,25 @@
 CREATE OR REPLACE VIEW documentsuggestiongroup AS
   SELECT
-    "authorship"."document"  AS "document",
-    "membership"."group" AS "researchEntity"
+    "authorship"."document" AS "document",
+    "membership"."group"    AS "researchEntity"
   FROM "membership"
-  JOIN "authorship" ON "membership"."user" = "authorship"."researchEntity"
-  JOIN "affiliation" ON "authorship"."id" = "affiliation"."authorship"
-  WHERE NOT EXISTS (
-    SELECT * FROM "authorshipgroup" WHERE "researchEntity" = "membership"."group" AND "document" = "authorship"."document"
-  )
-  AND NOT EXISTS (
-    SELECT * FROM "discardedgroup" WHERE "researchEntity" = "membership"."group" AND "document" = "authorship"."document"
-  )
-  AND "affiliation"."institute" = 1
-  --TODO: subsitute 1 with the default group id
+    JOIN "authorship"
+      ON "membership"."user" = "authorship"."researchEntity"
+    JOIN "affiliation"
+      ON "authorship"."id" = "affiliation"."authorship"
+  WHERE
+    NOT EXISTS(
+        SELECT *
+        FROM "authorshipgroup"
+        WHERE "researchEntity" = "membership"."group"
+              AND "document" = "authorship"."document"
+    )
+    AND NOT EXISTS(
+        SELECT *
+        FROM "discardedgroup"
+        WHERE "researchEntity" = "membership"."group"
+              AND "document" = "authorship"."document"
+    )
+    AND "affiliation"."institute" = 1
+--TODO: subsitute 1 with the default group id
 
