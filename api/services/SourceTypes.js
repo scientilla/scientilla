24a01386baee6a86bd55fc56c26cb2626da78083
iit-federico-bozzini <federico.bozzini@iit.it>
2017-03-29 14:43:02 +0200

    Added new accepted sources for invited talks.

--- api/services/SourceTypes.js
+++ api/services/SourceTypes.js
@@ -6,8 +6,12 @@ module.exports = {
     BOOK: 'book',
     JOURNAL: 'journal',
     CONFERENCE: 'conference',
+    SCIENTIFIC_CONFERENCE: 'scientific_conference',
     INSTITUTE: 'institute',
     BOOKSERIES: 'bookseries',
     WORKSHOP: 'workshop',
     SCHOOL: 'school',
+    MEDIA: 'media',
+    PUBLIC_EVENT: 'public_event',
+    OUTREACH: 'outreach'
 };
