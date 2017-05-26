b4b002ef0029a99ea639168e96ac66cc4837b3d1
federicobozzini <federico.bozzini@gmail.com>
2017-05-26 12:36:28 +0200

    Fixed bug on permissions.

--- api/policies/isActivityOwner.js
+++ api/policies/isActivityOwner.js
@@ -5,7 +5,8 @@ module.exports = function (req, res, next) {
         if (err) {
             return res.forbidden(err);
         }
-        if (req.params.researchEntityId != user.id && user.role != 'administrator'){
+        const userId = req.params.researchEntityId || req.params.id;
+        if (userId != user.id && user.role != 'administrator'){
             sails.log.debug('access forbidden ' + req.path);
             return res.forbidden(err);
         }
