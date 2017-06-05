f795ad27e1e4d4b5f430350c33e8963f4c9d91a7
federicobozzini <federico.bozzini@gmail.com>
2017-06-05 17:51:40 +0200

    DocumentMerger, added dome debugging logs.

--- api/services/DocumentMerger.js
+++ api/services/DocumentMerger.js
@@ -10,11 +10,16 @@ module.exports = {
 
 //missing check of discarded documents
 async function mergeDocuments() {
+    const mergedDocuments = [];
+    const nonMergedDocuments = [];
     const documents = await Document.findByKind(DocumentKinds.VERIFIED)
         .populate('authorships')
         .populate('groupAuthorships')
         .populate('affiliations');
+    sails.log.debug(`${documents.length} documents to check found`);
+
     for (let doc of documents) {
+        const errors = [];
         const copies = await Document.findCopies(doc);
         if (copies.length == 0)
             continue;
@@ -24,20 +29,27 @@ async function mergeDocuments() {
                 continue;
              const instituteIds = doc.affiliations.filter(aff => aff.authorship == a.id).map(aff => aff.institute);
             const res = await User.verifyDocument(User, a.researchEntity, copy.id, a.position, instituteIds, a.corresponding);
-            if (res.error)
+            if (res.error) {
+                errors.push(res);
                 sails.log.debug(`Document with id ${doc.id} has authorship problem. User: ${a.researchEntity}, position: ${a.position}`);
-            else
+            } else
                 await User.unverifyDocument(User, a.researchEntity, doc.id);
         }
         for (let a of doc.groupAuthorships) {
             if (!a.researchEntity)
                 continue;
             const res = await Group.verifyDocument(Group, a.researchEntity, copy.id, a.position, [], a.corresponding);
-            if (res.error)
+            if (res.error) {
+                errors.push(res);
                 sails.log.debug(`Document with id ${doc.id} has authorship problem. Group: ${a.researchEntity}, position: ${a.position}`);
-            else
+            } else
                 await Group.unverifyDocument(Group, a.researchEntity, doc.id);
         }
-
+        if (errors.length)
+            nonMergedDocuments.push(doc);
+        else
+            mergedDocuments.push(doc);
     }
+    sails.log.debug(`${mergedDocuments.length} documents merged`);
+    sails.log.debug(`${nonMergedDocuments.length} documents were not merged because of some error`);
 }
