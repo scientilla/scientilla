626ad8e391a87c1ed7b0111676ef1959c8f9ab85
fedesem2 <fedesem2@yahoo.it>
2017-06-13 14:14:58 +0200

    Refactor of docker/npm script

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+USERID=$UID
+export USERID
+
 if [ "$1" == "test" ]; then
     docker-compose -f docker-compose-tools.yml up -d db-test
-    docker-compose -f docker-compose-tools.yml run --rm mocha test/bootstrap.test.js test/integration/bootstrap.test.js test/**/*.test.js
+    docker-compose -f docker-compose-tools.yml run --rm npm test
 else
-    USERID=$UID
-    export USERID
     docker-compose -f docker-compose-tools.yml run --rm npm $*
 fi
