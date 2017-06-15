1b97d15d214017720f2a5b15e4da63520583a753
federicobozzini <federico.bozzini@gmail.com>
2017-06-15 18:43:29 +0200

    Comparison between invited talks is now handled properly for finding duplicates.

--- config/databaseViews/documentduplicate.sql
+++ config/databaseViews/documentduplicate.sql
@@ -12,15 +12,21 @@ CREATE OR REPLACE VIEW documentduplicate AS
     JOIN document dd
       ON a.document = dd.id
          OR u.id = dd."draftCreator"
-  WHERE ((d.doi = dd.doi) :: INT +
-         (d."authorsStr" = dd."authorsStr") :: INT +
-         (d.title = dd.title) :: INT +
-         (d."scopusId" = dd."scopusId") :: INT > 1)
-        AND D.id <> dd.id
+  WHERE
+         CASE WHEN d.type = 'invited_talk' OR dd.type = 'invited_talk'
+           THEN (d."authorsStr" = dd."authorsStr") :: INT +
+                (d.title = dd.title) :: INT +
+                (d."itSource" = dd."itSource") :: INT > 1
+         ELSE (d.doi = dd.doi) :: INT +
+              (d."authorsStr" = dd."authorsStr") :: INT +
+              (d.title = dd.title) :: INT +
+              (d."scopusId" = dd."scopusId") :: INT > 1
+         END
+        AND d.id <> dd.id
         AND (
-          (D.kind = 'd' AND dd.kind IN ('d', 'v'))
+          (d.kind = 'd' AND dd.kind IN ('d', 'v'))
           OR
-          (D.kind in ('v','e') AND dd.kind = 'v'))
+          (d.kind IN ('v', 'e') AND dd.kind = 'v'))
   UNION
   SELECT
     d.id    AS document,
@@ -28,19 +34,24 @@ CREATE OR REPLACE VIEW documentduplicate AS
     g.id    AS "researchEntity",
     'group' AS "researchEntityType",
     1       AS id
-  FROM document d,
-    "group" g
+  FROM "group" g
     LEFT JOIN authorshipgroup a
       ON g.id = a."researchEntity"
     JOIN document dd
       ON a.document = dd.id
          OR g.id = dd."draftGroupCreator"
-  WHERE ((d.doi = dd.doi) :: INT +
-         (d."authorsStr" = dd."authorsStr") :: INT +
-         (d.title = dd.title) :: INT +
-         (d."scopusId" = dd."scopusId") :: INT > 1)
-        AND D.id <> dd.id
-        AND (
-          (D.kind = 'd' AND dd.kind IN ('d', 'v'))
-          OR
-          (D.kind in ('v','e') AND dd.kind = 'v'))
+    JOIN document d
+      ON CASE WHEN d.type = 'invited_talk' OR dd.type = 'invited_talk'
+      THEN (d."authorsStr" = dd."authorsStr") :: INT +
+           (d.title = dd.title) :: INT +
+           (d."itSource" = dd."itSource") :: INT > 1
+         ELSE (d.doi = dd.doi) :: INT +
+              (d."authorsStr" = dd."authorsStr") :: INT +
+              (d.title = dd.title) :: INT +
+              (d."scopusId" = dd."scopusId") :: INT > 1
+         END
+         AND D.id <> dd.id
+         AND (
+           (D.kind = 'd' AND dd.kind IN ('d', 'v'))
+           OR
+           (D.kind IN ('v', 'e') AND dd.kind = 'v'))
