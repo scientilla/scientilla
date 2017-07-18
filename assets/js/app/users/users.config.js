d4369472a0e502c4193321fb228807810b12afc5
fedesem2 <fedesem2@yahoo.it>
2017-07-18 10:40:47 +0200

    Changed collaborations with groups

--- assets/js/app/users/users.config.js
+++ assets/js/app/users/users.config.js
@@ -1,34 +1,34 @@
 (function () {
     angular
-            .module('users')
-            .config(configure);
+        .module('users')
+        .config(configure);
 
     configure.$inject = ['$routeProvider'];
 
     function configure($routeProvider) {
         $routeProvider
-                .when("/users", {
-                    templateUrl: "partials/user-browsing.html",
-                    controller: "UserBrowsingController",
-                    controllerAs: 'vm',
-                })
-                .when("/users/:id", {
-                    templateUrl: "partials/user-details.html",
-                    controller: "UserDetailsController",
-                    controllerAs: 'vm',
-                    resolve: {
-                        user: getCurrentUser
-                    },
-                    access: {
-                        noLogin: true
-                    }
-                });
+            .when("/users", {
+                templateUrl: "partials/user-browsing.html",
+                controller: "UserBrowsingController",
+                controllerAs: 'vm',
+            })
+            .when("/users/:id", {
+                templateUrl: "partials/user-details.html",
+                controller: "UserDetailsController",
+                controllerAs: 'vm',
+                resolve: {
+                    user: getCurrentUser
+                },
+                access: {
+                    noLogin: true
+                }
+            });
     }
 
     getCurrentUser.$inject = ['UsersService', '$route'];
 
     function getCurrentUser(UsersService, $route) {
         var userId = $route.current.params.id;
-        return UsersService.one(userId).get({populate: ['collaborations', 'aliases']});
+        return UsersService.one(userId).get({populate: ['collaborations', 'aliases', 'memberships']});
     }
 })();
