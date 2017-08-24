9fc11f521c70b66ba5706c4572eece5b55f3de8c
federicobozzini <federico.bozzini@gmail.com>
2017-08-24 19:09:48 +0200

    Fixed bug on removal of source duplicates.

--- api/services/Cleaner.js
+++ api/services/Cleaner.js
@@ -20,8 +20,9 @@ async function cleanInstituteCopies() {
 
     // bad hack: Institute 1 is the main institute
     const mainInstituteId = 1;
-    const institutes = await Institute.find({id: {'!': mainInstituteId}, scopusId: {'!': null}});
-    sails.log.debug(`${institutes.length} to check`);
+    const allInstitutes = await Institute.find({id: {'!': mainInstituteId}, scopusId: {'!': null}});
+    const institutes = allInstitutes.filter(i => i.scopusId);
+    sails.log.info(`${institutes.length} institutes to check`);
     const deletedInstitutes = [];
     for (let institute of institutes) {
         const copy = await getInstituteCopy(institute);
@@ -32,12 +33,12 @@ async function cleanInstituteCopies() {
         if (affiliations.length) {
             const affiliationsIds = affiliations.map(a => a.id);
             const updateAffiliations = await Affiliation.update({id: affiliationsIds}, {institute: copy.id});
-            sails.log.debug(`${updateAffiliations.length} affiliations updated from institute ${institute.id} to ${copy.id}`);
+            sails.log.info(`${updateAffiliations.length} affiliations updated from institute ${institute.id} to ${copy.id}`);
         }
         await institute.destroy();
         deletedInstitutes.push(institute);
     }
-    sails.log.debug(`${deletedInstitutes.length} deleted`);
+    sails.log.info(`${deletedInstitutes.length} deleted`);
 
     const institutesDup = await Institute.find({id: {'!': mainInstituteId}, parentId: {'!': null}});
     for (let institute of institutesDup) {
@@ -49,7 +50,7 @@ async function cleanInstituteCopies() {
         if (affiliations.length) {
             const affiliationsIds = affiliations.map(a => a.id);
             const updateAffiliations = await Affiliation.update({id: affiliationsIds}, {institute: parent.id});
-            sails.log.debug(`${updateAffiliations.length} affiliations updated from institute ${institute.id} to ${parent.id}`);
+            sails.log.info(`${updateAffiliations.length} affiliations updated from institute ${institute.id} to ${parent.id}`);
         }
     }
 
@@ -60,8 +61,10 @@ async function cleanSourceCopies() {
         return Source.findOne({id: {'<': i.id}, scopusId: i.scopusId});
     }
 
-    const sources = await Source.find({scopusId: {'!': null}});
+    const allSources = await Source.find({scopusId: {'!': null}});
+    const sources = allSources.filter(s => s.scopusId);
     const deletedSources = [];
+    sails.log.info(`${sources.length} sources to check`);
     for (let source of sources) {
         const copy = await getSourceCopy(source);
         if (!copy)
@@ -71,19 +74,19 @@ async function cleanSourceCopies() {
         if (documents.length) {
             const documentIds = documents.map(d => d.id);
             const updateDocuments = await Document.update({id: documentIds}, {source: copy.id});
-            sails.log.debug(`${updateDocuments.length} documents updated from source ${source.id} to ${copy.id}`);
+            sails.log.info(`${updateDocuments.length} documents updated from source ${source.id} to ${copy.id}`);
         }
         await source.destroy();
         deletedSources.push(source);
     }
-    sails.log.debug(`${deletedSources.length} deleted`);
+    sails.log.info(`${deletedSources.length} deleted`);
 }
 
 async function cleanDocumentCopies() {
     const mergedDocuments = [];
     const nonMergedDocuments = [];
     const documents = await Document.findByKind(DocumentKinds.VERIFIED);
-    sails.log.debug(`${documents.length} documents to check found`);
+    sails.log.info(`${documents.length} documents to check found`);
 
     for (let partialDoc of documents) {
         const doc = await Document.findOneById(partialDoc.id)
@@ -106,25 +109,25 @@ async function cleanDocumentCopies() {
             continue;
         const copy = copies[0];
         for (let d of doc.discarded) {
-            sails.log.debug(`User ${d.researchEntity} is removing ${doc.id} from the discarded`);
+            sails.log.info(`User ${d.researchEntity} is removing ${doc.id} from the discarded`);
             const res = await User.undiscardDocument(User, d.researchEntity, d.document);
             if (res.error) {
                 errors.push(res);
                 sails.log.warn('Error: ');
                 sails.log.warn(res.error);
             } else {
-                sails.log.debug(`User ${d.researchEntity} is discarding ${copy.id}`);
+                sails.log.info(`User ${d.researchEntity} is discarding ${copy.id}`);
                 await User.discardDocument(User, d.researchEntity, copy.id);
             }
         }
         for (let d of doc.discardedG) {
-            sails.log.debug(`Group ${d.researchEntity} is removing ${doc.id} from the discarded`);
+            sails.log.info(`Group ${d.researchEntity} is removing ${doc.id} from the discarded`);
             const res = await Group.undiscardDocument(Group, d.researchEntity, d.document);
             if (res.error) {
                 sails.log.warn('Error: ');
                 sails.log.warn(res.error);
             } else {
-                sails.log.debug(`Group ${d.researchEntity} is discarding ${copy.id}`);
+                sails.log.info(`Group ${d.researchEntity} is discarding ${copy.id}`);
                 await Group.discardDocument(Group, d.researchEntity, copy.id);
             }
         }
@@ -132,7 +135,7 @@ async function cleanDocumentCopies() {
             if (!a.researchEntity)
                 continue;
             const instituteIds = doc.affiliations.filter(aff => aff.authorship === a.id).map(aff => aff.institute);
-            sails.log.debug(`User ${a.researchEntity} is trying to verify document ${copy.id} (${copy.title}) in position: ${a.position}`);
+            sails.log.info(`User ${a.researchEntity} is trying to verify document ${copy.id} (${copy.title}) in position: ${a.position}`);
             const res = await User.verifyDocument(User, a.researchEntity, copy.id, {
                 position: a.position,
                 affiliationInstituteIds: instituteIds,
@@ -144,33 +147,33 @@ async function cleanDocumentCopies() {
                 sails.log.warn('Error: ');
                 sails.log.warn(res.error);
             } else {
-                sails.log.debug(`User ${a.researchEntity} is trying to unverify document ${doc.id} (${doc.title})`);
+                sails.log.info(`User ${a.researchEntity} is trying to unverify document ${doc.id} (${doc.title})`);
                 await User.unverifyDocument(User, a.researchEntity, doc.id);
             }
         }
         for (let a of doc.groupAuthorships) {
             if (!a.researchEntity)
                 continue;
-            sails.log.debug(`Group ${a.researchEntity} is trying to verify document ${copy.id} (${copy.title})`);
+            sails.log.info(`Group ${a.researchEntity} is trying to verify document ${copy.id} (${copy.title})`);
             const res = await Group.verifyDocument(Group, a.researchEntity, copy.id);
             if (res.error) {
                 errors.push(res);
                 sails.log.warn('Error: ');
                 sails.log.warn(res.error);
             } else {
-                sails.log.debug(`Group ${a.researchEntity} is trying to unverify document ${doc.id} (${doc.title})`);
+                sails.log.info(`Group ${a.researchEntity} is trying to unverify document ${doc.id} (${doc.title})`);
                 await Group.unverifyDocument(Group, a.researchEntity, doc.id);
             }
         }
         if (errors.length) {
-            sails.log.debug(`Document ${doc.id} should have been merged with  ${copy.id} but an error occured`)
+            sails.log.info(`Document ${doc.id} should have been merged with  ${copy.id} but an error occured`)
             nonMergedDocuments.push({doc: doc, copy: copy});
         }
         else {
-            sails.log.debug(`Document ${doc.id} was merged with ${copy.id}`);
+            sails.log.info(`Document ${doc.id} was merged with ${copy.id}`);
             mergedDocuments.push({doc: doc, copy: copy});
         }
     }
-    sails.log.debug(`${mergedDocuments.length} documents merged`);
-    sails.log.debug(`${nonMergedDocuments.length} documents were not merged because of some error`);
+    sails.log.info(`${mergedDocuments.length} documents merged`);
+    sails.log.info(`${nonMergedDocuments.length} documents were not merged because of some error`);
 }
