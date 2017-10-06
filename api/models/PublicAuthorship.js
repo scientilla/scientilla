14619152ffdc915afae3f8f779232cf69a5a4d4e
federicobozzini <federico.bozzini@gmail.com>
2017-10-06 18:43:53 +0200

    Added additional models for the public documents API.

---
+++ api/models/PublicAuthorship.js
@@ -0,0 +1,26 @@
+/**
+ * PublicAuthorship.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        document: {
+            model: 'document',
+            primaryKey: true
+        },
+        researchEntity: {
+            model: 'user',
+            primaryKey: true
+        }
+    },
+
+    migrate: 'safe',
+    tableName: 'publicauthorship',
+    autoUpdatedAt: false,
+    autoCreatedAt: false
+};
+
---
+++ api/models/PublicAuthorshipGroup.js
@@ -0,0 +1,26 @@
+/**
+ * PublicAuthorshipGroup.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        document: {
+            model: 'document',
+            primaryKey: true
+        },
+        researchEntity: {
+            model: 'group',
+            primaryKey: true
+        }
+    },
+
+    migrate: 'safe',
+    tableName: 'publicauthorshipgroup',
+    autoUpdatedAt: false,
+    autoCreatedAt: false
+};
+
