dc3a37da1a3bf28fe7fe74db7e934be8932c795b
federicobozzini <federico.bozzini@gmail.com>
2017-10-19 17:34:59 +0200

    Document type is return for public apis.

--- api/lib/ResearchEntity.js
+++ api/lib/ResearchEntity.js
@@ -245,7 +245,7 @@ module.exports = _.merge({}, BaseModel, {
             throw Error('404 page not found');
         const baseUrl = sails.getBaseUrl();
         const path = `/api/v1/${researchEntity.getUrlSection()}/${researchEntity.id}/${attribute}`;
-        qs.populate = ['source', 'affiliations', 'authorships', 'institutes'];
+        qs.populate = ['source', 'affiliations', 'authorships', 'institutes', 'documenttype'];
         const reqOptions = {
             uri: baseUrl + path,
             json: true,
