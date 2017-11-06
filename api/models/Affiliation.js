b561e5a1c74b770117f244c420427246dcd1aff9
fedesem2 <fedesem2@yahoo.it>
2017-11-06 20:19:52 +0100

    Refactor

--- api/models/Affiliation.js
+++ api/models/Affiliation.js
@@ -1,9 +1,5 @@
-/**
- * Affiliation.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
+/* global Authorship, Affiliation*/
+"use strict";
 
 module.exports = {
 
