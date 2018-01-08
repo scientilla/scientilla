c60f34ae5a46fab7d2cbaf4702bd8dcfd3cab7c4
federicobozzini <federico.bozzini@gmail.com>
2018-01-08 18:10:46 +0100

    Suggested documents are now automatically filtered out by year.

--- config/databaseViews/documentsuggestiongroup.sql
+++ config/databaseViews/documentsuggestiongroup.sql
@@ -7,6 +7,8 @@ CREATE OR REPLACE VIEW documentsuggestiongroup AS
       ON "membership"."user" = "authorship"."researchEntity"
     JOIN "affiliation"
       ON "authorship"."id" = "affiliation"."authorship"
+    JOIN document d
+      ON authorship.document = d.id
   WHERE
     NOT EXISTS(
         SELECT *
@@ -21,5 +23,6 @@ CREATE OR REPLACE VIEW documentsuggestiongroup AS
               AND "document" = "authorship"."document"
     )
     AND "affiliation"."institute" = 1
+    AND NULLIF(d.year, '')::int > 2006
 --TODO: subsitute 1 with the default group id
 
