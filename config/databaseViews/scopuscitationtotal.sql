1379e3e508795e35c809965c11cb17c5e89b84e8
fedesem2 <fedesem2@yahoo.it>
2018-01-12 20:21:24 +0100

    Removed unused views

--- config/databaseViews/scopuscitationtotal.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW scopuscitationtotal AS (
-  SELECT
-    sc.document      AS document,
-    sum(c.citations) AS citations
-  FROM scopuscitation sc
-    JOIN citation c
-      ON sc.citation = c.id
-  GROUP BY sc.document
-);
