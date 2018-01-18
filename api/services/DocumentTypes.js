7139d01228b9b0eba0044297cba2576b92c6b078
fedesem2 <fedesem2@yahoo.it>
2018-01-18 18:09:03 +0100

    Fixed bug that was deleting DocumentTypes objects

--- api/services/DocumentTypes.js
+++ api/services/DocumentTypes.js
@@ -6,23 +6,6 @@
 let documentTypes = [];
 
 module.exports = {
-    ARTICLE: {},
-    ARTICLE_IN_PRESS: {},
-    ABSTRACT_REPORT: {},
-    BOOK: {},
-    BOOK_CHAPTER: {},
-    CONFERENCE_PAPER: {},
-    CONFERENCE_REVIEW: {},
-    EDITORIAL: {},
-    ERRATUM: {},
-    INVITED_TALK: {},
-    LETTER: {},
-    NOTE: {},
-    REPORT: {},
-    REVIEW: {},
-    SHORT_SURVEY: {},
-    PHD_THESIS: {},
-    POSTER: {},
     init: async () => {
         documentTypes = await DocumentType.find();
         documentTypes.forEach(dt => DocumentTypes[dt.key.toLocaleUpperCase()] = dt);
