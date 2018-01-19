cee684c6e60cfa3bcf018f2de040a8052495f20d
federicobozzini <federico.bozzini@gmail.com>
2018-01-19 16:23:52 +0100

    Added defensive check.

--- assets/js/app/documents/doi.filter.js
+++ assets/js/app/documents/doi.filter.js
@@ -5,6 +5,8 @@
     function doiFilter() {
 
         function getDoi(document) {
+            if (!document)
+                return '';
             var doi = document.doi;
             var titleStr = doi ? '<b>DOI:</b> <a href="http://dx.doi.org/' + doi + '" target="_blank" >' + doi + '</a>' : '';
             return titleStr;
