466a5ba75d51661c64db246833b3a47a4258a5a7
fedesem2 <fedesem2@yahoo.it>
2018-02-19 14:28:17 +0100

    Added authorships legend

--- assets/js/app/documents/scientilla-document-details.component.js
+++ assets/js/app/documents/scientilla-document-details.component.js
@@ -19,6 +19,10 @@
     function scientillaDocumentsDetails(documentTypes, documentSourceTypes) {
         const vm = this;
 
+        vm.hasCorresponding = hasCorresponding;
+        vm.hasCoFirst = hasCoFirst;
+        vm.hasCoLast = hasCoLast;
+        vm.hasOral = hasOral;
 
         vm.$onInit = function () {
             vm.type = _.get(documentTypes.find(dt => dt.key === vm.document.type), 'label');
@@ -39,6 +43,22 @@
             if (vm.document.pages)
                 vm.bibliographicInformations.push('pp. ' + vm.document.pages);
         };
+
+        function hasCorresponding() {
+            return !!vm.document.authorships.find(a => a.corresponding);
+        }
+
+        function hasCoFirst() {
+            return !!vm.document.authorships.find(a => a.first_coauthor);
+        }
+
+        function hasCoLast() {
+            return !!vm.document.authorships.find(a => a.last_coauthor);
+        }
+
+        function hasOral() {
+            return !!vm.document.authorships.find(a => a.oral_presentation);
+        }
     }
 
 })();
