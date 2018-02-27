32d276cc7feead32bedd3983841919692b04c1e2
fedesem2 <fedesem2@yahoo.it>
2018-02-27 18:23:57 +0100

    favorite publications and oral presentations are now only public publications

--- config/databaseViews/favoritepublication.sql
+++ config/databaseViews/favoritepublication.sql
@@ -1,6 +1,11 @@
 CREATE OR REPLACE VIEW favoritepublication AS
   SELECT
-    "researchEntity",
-    document
-  FROM authorship
-  WHERE favorite = TRUE
+    a."researchEntity",
+    a.document
+  FROM authorship a
+    JOIN document d ON a.document = d.id
+    JOIN source s ON d.source = s.id
+    JOIN sourcetype st ON s.sourcetype = st.id
+  WHERE a.favorite = TRUE
+        AND a.public = TRUE
+        AND st.type = 'publication'
--- config/databaseViews/oralpresentation.sql
+++ config/databaseViews/oralpresentation.sql
@@ -1,6 +1,11 @@
 CREATE OR REPLACE VIEW oralpresentation AS
   SELECT
-    "researchEntity",
-    document
-  FROM authorship
+    a."researchEntity",
+    a.document
+  FROM authorship a
+    JOIN document d ON a.document = d.id
+    JOIN source s ON d.source = s.id
+    JOIN sourcetype st ON s.sourcetype = st.id
   WHERE oral_presentation = TRUE
+        AND a.public = TRUE
+        AND st.type = 'publication'
