005b012280b8c87c0606d1e0ad0fcf4021d736b0
fedesem2 <fedesem2@yahoo.it>
2018-05-14 20:43:21 +0200

    Removed watched folders

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
 //        },
         assets: {
             // Assets to watch:
-            files: ['assets/**/*', 'tasks/pipeline.js', '!**/node_modules/**'],
+            files: ['assets/**/*'],
             // When assets are changed:
             tasks: ['compileAssets', 'linkAssets']
         },
