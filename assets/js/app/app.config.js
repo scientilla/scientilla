1c51b94e9cf239b352daf11b4f5ba4c771546d49
Dieter Casier <dieter.casier@iit.it>
2018-10-03 11:14:26 +0200

    Close modal when url has changed

--- assets/js/app/app.config.js
+++ assets/js/app/app.config.js
@@ -44,7 +44,6 @@
             const goingToNoRedirectUrl = next.$$route && noRedirectUrls.includes(next.$$route.originalPath);
             if (!goingToNoRedirectUrl && !AuthService.isAvailable && !AuthService.isAdmin) {
                 Notification.warning('Sorry but scientilla is temporarly unavailable. Try again later.');
-                ModalService.dismiss(null);
                 path.goTo('/unavailable');
                 return;
             }
@@ -55,6 +54,7 @@
                     path.goTo("/login");
                 }
             }
+            ModalService.dismiss(null);
         });
 
         $rootScope.$on('$locationChangeSuccess', (event, current) =>
