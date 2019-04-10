7c084fa519722769253dd6e16464d9aca4b44b19
Dieter Casier <casier.dieter@gmail.com>
2019-04-10 12:16:08 +0200

    Remove docker-compose & docker files

--- docker/db-test/Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM postgres:10
-
-ADD config.sh /docker-entrypoint-initdb.d/
--- docker/db-test/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-sed -ri "s/#log_statement = 'none'/log_statement = 'all'/g" /var/lib/postgresql/data/postgresql.conf
