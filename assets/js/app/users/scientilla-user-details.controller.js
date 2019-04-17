5669359da226cd6be50b26439c355d09b25512ba
Dieter Casier <casier.dieter@gmail.com>
2019-04-17 15:18:54 +0200

    Add accomplishments tab to profile

--- assets/js/app/users/scientilla-user-details.controller.js
+++ assets/js/app/users/scientilla-user-details.controller.js
@@ -12,23 +12,27 @@
         });
 
     controller.$inject = [
+        'context',
         'UsersService',
         'documentListSections',
+        'accomplishmentListSections',
         'AuthService',
         '$scope',
         '$controller'
     ];
 
-    function controller(UsersService, documentListSections, AuthService, $scope, $controller) {
+    function controller(context, UsersService, documentListSections, accomplishmentListSections, AuthService, $scope, $controller) {
         const vm = this;
         angular.extend(vm, $controller('SummaryInterfaceController', {$scope: $scope}));
 
         vm.documentListSections = documentListSections;
+        vm.accomplishmentListSections = accomplishmentListSections;
         vm.loggedUser = AuthService.user;
 
         /* jshint ignore:start */
         vm.$onInit = async () => {
             vm.user = await UsersService.getUser(vm.userId);
+            vm.researchEntity = await context.getResearchEntity();
             vm.chartsData = await vm.getChartsData(vm.user);
         };
         /* jshint ignore:end */
