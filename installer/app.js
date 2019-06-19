7ec920b17f96a9444641c5a3876edf493ec89856
Dieter Casier <casier.dieter@gmail.com>
2019-06-19 12:49:58 +0200

    Set default db configuration from env

--- installer/app.js
+++ installer/app.js
@@ -503,7 +503,28 @@ async function initialize() {
         const stats = fs.statSync(localConfigurationFile)
 
         if (stats.size == 0) {
-            copyFile(defaultLocalConfigurationFile, configFolder)
+            const localJs = {
+                production:{
+                    "adapter": "sails-postgresql",
+                    "host": process.env.DATABASE_HOST,
+                    "user": process.env.DATABASE_USER,
+                    "password": process.env.DATABASE_PASSWORD,
+                    "database": process.env.DATABASE_NAME
+                },
+                staging:{
+                    "adapter": "sails-postgresql",
+                    "host": process.env.DATABASE_HOST,
+                    "user": process.env.DATABASE_USER,
+                    "password": process.env.DATABASE_PASSWORD,
+                    "database": process.env.DATABASE_NAME
+                }
+            }
+            fs.writeFile(basicConfigurationFile, prefixLocal + JSON.stringify(localJs, null, 4), (err) => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            })
         }
     }
 
