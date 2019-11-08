abbbbd004e6828025a59260c24a60f9e8c3a8fe7
Dieter Casier <casier.dieter@gmail.com>
2019-11-08 14:33:37 +0100

    Re-add watch to default task

--- tasks/register/default.js
+++ tasks/register/default.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
 	grunt.registerTask('default',
-        ['jshint', 'compileAssets', 'linkAssets']
+        ['jshint', 'compileAssets', 'linkAssets', 'watch']
     );
 };
