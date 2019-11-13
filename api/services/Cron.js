5cc217b853eb0c65e74127b33ba38c52eb93b688
Dieter Casier <casier.dieter@gmail.com>
2019-11-13 12:14:12 +0100

    Change onTick function cron jobs

--- api/services/Cron.js
+++ api/services/Cron.js
@@ -8,22 +8,26 @@ const crons = [];
 
 module.exports = {
     start: () => {
-        sails.config.scientilla.crons.forEach(cron =>
-            crons.push(new CronJob(cron.time, async cron => {
-                if (!cron.enabled) {
-                    return;
-                }
-
-                sails.log.info('Cron ' + cron.name + ' started at ' + new Date().toISOString());
-
-                await GruntTaskRunner.run('Cron:' + cron.name);
-
-                sails.log.info('Cron ' + cron.name + ' finished at ' + new Date().toISOString());
-            }, null, false, 'Europe/Rome'))
-        );
+        sails.config.scientilla.crons.forEach(cron => {
+            crons.push(new CronJob(cron.time, onTick(cron), null, false, 'Europe/Rome'))
+        });
         crons.forEach(cron => cron.start());
     },
     stop: () => {
         crons.forEach(cron => cron.stop());
     }
 };
+
+function onTick(cron) {
+    return async function () {
+        if (!cron.enabled) {
+            return;
+        }
+
+        sails.log.info('Cron ' + cron.name + ' started at ' + new Date().toISOString());
+
+        await GruntTaskRunner.run('cron:' + cron.name);
+
+        sails.log.info('Cron ' + cron.name + ' finished at ' + new Date().toISOString());
+    }
+}
