0279736dcf98fe13e338ddde7f8d02fdf89a6cfa
Dieter Casier <casier.dieter@gmail.com>
2019-11-13 10:29:53 +0100

    Add cron task, update cron service

--- api/services/Cron.js
+++ api/services/Cron.js
@@ -9,7 +9,17 @@ const crons = [];
 module.exports = {
     start: () => {
         sails.config.scientilla.crons.forEach(cron =>
-            crons.push(new CronJob(cron.time, generateOnTick(cron), onStop, false, 'Europe/Rome'))
+            crons.push(new CronJob(cron.time, async cron => {
+                if (!cron.enabled) {
+                    return;
+                }
+
+                sails.log.info('Cron ' + cron.name + ' started at ' + new Date().toISOString());
+
+                await GruntTaskRunner.run('Cron:' + cron.name);
+
+                sails.log.info('Cron ' + cron.name + ' finished at ' + new Date().toISOString());
+            }, null, false, 'Europe/Rome'))
         );
         crons.forEach(cron => cron.start());
     },
@@ -17,26 +27,3 @@ module.exports = {
         crons.forEach(cron => cron.stop());
     }
 };
-
-function generateOnTick(cron) {
-    return async function () {
-        if (!cron.enabled)
-            return;
-
-        sails.log.info('Cron ' + cron.name + ' started at ' + new Date().toISOString());
-
-        for (const job of cron.jobs) {
-            try {
-                await _.get(global, job.fn)(...job.params);
-            } catch (e) {
-                sails.log.error(e);
-            }
-        }
-
-        sails.log.info('Cron ' + cron.name + ' finished at ' + new Date().toISOString());
-    }
-}
-
-function onStop() {
-
-}
