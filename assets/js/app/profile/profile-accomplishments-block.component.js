23ba2d45b7a10cdb01c5af148c4c606c84b6f10e
Dieter Casier <casier.dieter@gmail.com>
2020-01-27 15:40:55 +0100

    Add links to only favorite documents & accomplishments

--- assets/js/app/profile/profile-accomplishments-block.component.js
+++ assets/js/app/profile/profile-accomplishments-block.component.js
@@ -11,9 +11,9 @@
             }
         });
 
-    profileAccomplishmentsBlock.$inject = ['$uibModal', '$scope', 'context'];
+    profileAccomplishmentsBlock.$inject = ['context'];
 
-    function profileAccomplishmentsBlock($uibModal, $scope, context) {
+    function profileAccomplishmentsBlock(context) {
         const vm = this;
 
         vm.accomplishmentsByType = [];
@@ -22,50 +22,6 @@
         vm.$onInit = function () {
             vm.accomplishmentsByType = getAccomplishmentsByType(vm.profile);
             vm.favoriteAccomplishments = getFavoriteAccomplishments(vm.profile);
-        };
-
-        vm.showAccomplishmentsModal = () => {
-            $uibModal.open({
-                animation: true,
-                template:
-                    `<div class="modal-header">
-                            <h3 class="text-capitalize">Accomplishments</h3>
-                            <button
-                                type="button"
-                                class="close"
-                                ng-click="close()">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-
-                        <div class="modal-body profile">
-                            <ul class="accomplishment-categories">
-                                <li ng-repeat="(type, accomplishments) in vm.accomplishmentsByType">
-                                    <span class="accomplishment-category">{{ type }}</span>
-                                    <ul class="accomplishment-listing">
-                                        <li ng-repeat="accomplishment in accomplishments">
-                                            <h4 class="accomplishment-title">{{ accomplishment.title }}</h4>
-                                            <ul class="accomplishment-details">
-                                                <li ng-if="accomplishment.issuer">
-                                                    <strong>Issuer: </strong>{{ accomplishment.issuer }}
-                                                </li>
-                                                <li ng-if="accomplishment.year">
-                                                    <strong>Year: </strong>{{ accomplishment.year }}
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>`,
-                scope: $scope,
-                controller: ($scope, $uibModalInstance) => {
-                    $scope.close = () => {
-                        $uibModalInstance.close();
-                    };
-                },
-                size: 'lg'
-            });
         };
 
         function getAccomplishmentsByType(profile) {
--- assets/js/app/profile/profile-documents-block.component.js
+++ assets/js/app/profile/profile-documents-block.component.js
@@ -13,9 +13,9 @@
             }
         });
 
-    profileDocumentsBlock.$inject = ['$uibModal', '$scope', 'context'];
+    profileDocumentsBlock.$inject = ['context'];
 
-    function profileDocumentsBlock($uibModal, $scope, context) {
+    function profileDocumentsBlock(context) {
         const vm = this;
 
         vm.documentsBySourceType = [];
@@ -24,49 +24,6 @@
         vm.$onInit = function () {
             vm.documentsBySourceType = getDocumentsBySourceType(vm.profile);
             vm.favoriteDocuments = getFavoriteDocuments(vm.profile);
-        };
-
-        vm.showDocumentsModal = () => {
-            $uibModal.open({
-                animation: true,
-                template:
-                    `<div class="modal-header">
-                            <h3 class="text-capitalize">Documents</h3>
-                            <button
-                                type="button"
-                                class="close"
-                                ng-click="close()">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-
-                        <div class="modal-body profile">
-                            <ul class="document-categories">
-                                <li ng-repeat="(sourceType, documents) in vm.documentsBySourceType">
-                                    <span class="document-category">{{ sourceType }}</span>
-                                    <ul class="document-listing">
-                                        <li ng-repeat="document in documents">
-                                            <h4 class="document-title">{{ document.title}}</h4>
-                                            <div class="document-source">
-                                                <i ng-class="vm.getSourceTypeIcon(sourceType)" title="{{ sourceType }}"></i>
-                                                <span>{{ document.source.title }}</span>
-                                            </div>
-                                            <ul class="document-details" ng-if="document.doi">
-                                                <li><strong>DOI: </strong><a href="#">{{ document.doi }}</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>`,
-                scope: $scope,
-                controller: ($scope, $uibModalInstance) => {
-                    $scope.close = () => {
-                        $uibModalInstance.close();
-                    };
-                },
-                size: 'lg'
-            });
         };
 
         function getDocumentsBySourceType(profile) {
