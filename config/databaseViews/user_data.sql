0fdac3d9d431b96ea66bedf78e6d64208a2374a1
Dieter Casier <casier.dieter@gmail.com>
2020-02-05 12:27:14 +0100

    Update user_data view: add join user table

--- config/databaseViews/user_data.sql
+++ config/databaseViews/user_data.sql
@@ -1,7 +1,10 @@
 CREATE OR REPLACE VIEW user_data AS
-SELECT red id,
-       red.profile,
-       red.imported_data,
-       red.research_entity AS research_entity
+SELECT
+  red.id as id,
+  red.profile,
+  red.imported_data,
+  u.id as "user",
+  re.id as "research_entity"
 FROM research_entity_data red
   JOIN research_entity re on red.research_entity = re.id
+  JOIN "user" u on re.id = u.research_entity
