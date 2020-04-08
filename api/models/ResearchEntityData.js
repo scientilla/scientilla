502059157bfad46f7a2ae9b54a69dca7ae7bca6b
Dieter Casier <casier.dieter@gmail.com>
2020-04-08 14:32:44 +0200

    Update order of experiences after saving, change text

--- api/models/ResearchEntityData.js
+++ api/models/ResearchEntityData.js
@@ -1234,6 +1234,30 @@ function setupProfile(userData) {
                     break;
             }
 
+            userData.profile.experiencesExternal = _.orderBy(
+                userData.profile.experiencesExternal,
+                [
+                    experience => new moment(experience.from, ISO8601Format),
+                    experience => new moment(experience.to, ISO8601Format)
+                ],
+                [
+                    'desc',
+                    'desc'
+                ]
+            );
+
+            userData.profile.experiencesInternal = _.orderBy(
+                userData.profile.experiencesInternal,
+                [
+                    experience => new moment(experience.from, ISO8601Format),
+                    experience => new moment(experience.to, ISO8601Format)
+                ],
+                [
+                    'desc',
+                    'desc'
+                ]
+            );
+
             userData.profile.experiences = _.orderBy(
                 userData.profile.experiences,
                 [
@@ -1469,6 +1493,43 @@ async function saveProfile(req) {
                 profile.image.value = researchEntityData.profile.image.value;
             }
         }
+
+        // Sorting experiences
+        profile.experiencesExternal = _.orderBy(
+            profile.experiencesExternal,
+            [
+                experience => new moment(experience.from, ISO8601Format),
+                experience => new moment(experience.to, ISO8601Format)
+            ],
+            [
+                'desc',
+                'desc'
+            ]
+        );
+
+        profile.experiencesInternal = _.orderBy(
+            profile.experiencesInternal,
+            [
+                experience => new moment(experience.from, ISO8601Format),
+                experience => new moment(experience.to, ISO8601Format)
+            ],
+            [
+                'desc',
+                'desc'
+            ]
+        );
+
+        profile.experiences = _.orderBy(
+            profile.experiences,
+            [
+                experience => new moment(experience.from, ISO8601Format),
+                experience => new moment(experience.to, ISO8601Format)
+            ],
+            [
+                'desc',
+                'desc'
+            ]
+        );
 
         // If the profile has some errors
         if (!valid || validate.errors) {
