9b9120f5ea216fda7d8617fc36c23d8dcb71f08a
fedesem2 <fedesem2@yahoo.it>
2020-06-04 16:05:01 +0200

    Added documentation of the projects

---
+++ assets/js/app/documentation/documentation-project-object-table.component.js
@@ -0,0 +1,17 @@
+/* global angular */
+(function () {
+    angular
+        .module('documentation')
+        .component('documentationProjectObjectTable', {
+            controller: controller,
+            templateUrl: 'partials/documentation-project-object-table.html',
+            controllerAs: 'vm',
+            bindings: {
+
+            }
+        });
+
+    function controller() {
+    }
+
+})();
