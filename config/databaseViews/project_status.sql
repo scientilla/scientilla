af4d1ad7c1659d3a1461b98f0f9868694efe4195
Dieter Casier <casier.dieter@gmail.com>
2020-07-31 16:12:11 +0200

    Update db view project status

--- config/databaseViews/project_status.sql
+++ config/databaseViews/project_status.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW project_status AS
-SELECT id, status
-FROM project
-WHERE status IS NOT NULL
-GROUP BY status;
+  SELECT row_number() OVER () AS id, status
+  FROM project
+  WHERE status IS NOT NULL
+  GROUP BY status;
