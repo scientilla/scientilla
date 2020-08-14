2975425378fb1ba3cce18c524ec11cac71df9ff3
Dieter Casier <casier.dieter@gmail.com>
2020-08-14 10:52:06 +0200

    Add annual contribution to project detail/modal

--- assets/js/app/projects/scientilla-project-details.component.js
+++ assets/js/app/projects/scientilla-project-details.component.js
@@ -11,14 +11,41 @@
             }
         });
 
-    scientillaProjectDetails.$inject = [];
+    scientillaProjectDetails.$inject = ['context', 'AuthService'];
 
-    function scientillaProjectDetails() {
+    function scientillaProjectDetails(context, AuthService) {
         const vm = this;
 
-        vm.$onInit = function () {
+        /* jshint ignore:start */
+        vm.$onInit = async function () {
 
+            vm.showAnnualContribution = isVerifiedUserOrGroup();
+
+            vm.PIMembers = vm.project.projectData.members.filter(m => {
+                const pi = vm.project.pi.find(pi => pi.email === m.email);
+                if (pi) {
+                    return m;
+                }
+            });
         };
+
+        async function isVerifiedUserOrGroup() {
+            vm.subResearchEntity = await context.getSubResearchEntity();
+            vm.user = AuthService.user;
+
+            if (vm.user.isAdmin()) {
+                return true;
+            }
+
+            if (vm.subResearchEntity.getType() === 'group') {
+                return vm.project.verifiedGroups.find(g => g.id === vm.subResearchEntity.id);
+            } else {
+                return vm.project.verifiedUsers.find(u => u.id === vm.subResearchEntity.id);
+            }
+
+            return false;
+        }
+        /* jshint ignore:end */
     }
 
 })();
