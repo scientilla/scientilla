910c61ff4b8d347d3d069760fd7c60bed794b6c5
Dieter Casier <casier.dieter@gmail.com>
2020-09-02 15:50:53 +0200

    Fix default binding research-entity for accomplishment views

--- assets/js/app/accomplishments/accomplishments.config.js
+++ assets/js/app/accomplishments/accomplishments.config.js
@@ -13,6 +13,7 @@
                     '<scientilla-accomplishment-suggested-list research-entity="$resolve.researchEntity">' +
                     '</scientilla-accomplishment-suggested-list>',
                 resolve: {
+                    researchEntity: getResearchEntity,
                     authService: getAuthService
                 }
             })
@@ -22,18 +23,26 @@
                     '<scientilla-accomplishment-verified-list research-entity="$resolve.researchEntity">' +
                     '</scientilla-accomplishment-verified-list>',
                 resolve: {
+                    researchEntity: getResearchEntity,
                     authService: getAuthService
                 }
             })
             .when("/:group?/accomplishments/drafts", {
                 controller: 'requestHandler',
                 template: params => '' +
-                    '<scientilla-accomplishment-drafts-list>' +
+                    '<scientilla-accomplishment-drafts-list research-entity="$resolve.researchEntity">' +
                     '</scientilla-accomplishment-drafts-list>',
                 resolve: {
+                    researchEntity: getResearchEntity,
                     authService: getAuthService
                 }
             });
+
+        getResearchEntity.$inject = ['context'];
+
+        function getResearchEntity(context) {
+            return context.getResearchEntity();
+        }
 
         getAuthService.$inject = ['AuthService'];
 
