f19c151d2642b7e24f3187cc39f61250df35e171
Dieter Casier <casier.dieter@gmail.com>
2020-09-17 08:52:47 +0200

    Update db view min max years: use year instead of date

--- config/databaseViews/research_item_min_max_year.sql
+++ config/databaseViews/research_item_min_max_year.sql
@@ -5,8 +5,8 @@ SELECT
        rit.type as item_type,
        rit.key as item_key,
        count(p) as count,
-       MIN(p.start_date) AS min,
-       MAX(p.start_date) AS max
+       MIN(p.start_year) AS min,
+       MAX(p.start_year) AS max
 FROM verify v
        JOIN project p on v.research_item = p.id
        JOIN research_item_type rit ON p.type = rit.id
@@ -20,8 +20,8 @@ SELECT
        rit.type as item_type,
        'all' as item_key,
        count(p) as count,
-       MIN(p.start_date) AS min,
-       MAX(p.start_date) AS max
+       MIN(p.start_year) AS min,
+       MAX(p.start_year) AS max
 FROM verify v
        JOIN project p on v.research_item = p.id
        JOIN research_item_type rit ON p.type = rit.id
@@ -30,7 +30,7 @@ GROUP BY research_entity, item_type, item_key
 UNION ALL
 
 SELECT
-        1 as id,
+       1 as id,
        v.research_entity as research_entity,
        rit.type as item_type,
        rit.key as item_key,
@@ -45,7 +45,7 @@ GROUP BY research_entity, item_key, item_type
 UNION ALL
 
 SELECT
-        1 as id,
+       1 as id,
        v.research_entity as research_entity,
        rit.type as item_type,
        'all' as item_key,
