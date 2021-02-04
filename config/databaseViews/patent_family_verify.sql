0e47b8632548443384e5266516d1934065ecc649
fedesem2 <fedesem2@yahoo.it>
2021-02-04 18:22:11 +0100

    Fixed wrong patent_family_verify join

--- config/databaseViews/patent_family_verify.sql
+++ config/databaseViews/patent_family_verify.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW patent_family_verify AS
-SELECT distinct rip.code as patent_family,
+SELECT distinct p.family_id as patent_family,
                 re.id    as research_entity
 FROM verify v
          JOIN research_entity re on v.research_entity = re.id
-         JOIN research_item_patent rip on v.research_item = rip.research_item;
+         JOIN patent p on v.research_item = p.id;
