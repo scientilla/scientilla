1b868cc7d9f51e4ea6cb900e5508c3a2e4ea713e
Dieter Casier <casier.dieter@gmail.com>
2021-03-04 15:48:53 +0100

    Refactor agreement detail component: remove unused code

--- assets/js/app/agreements/scientilla-agreement-details.component.js
+++ assets/js/app/agreements/scientilla-agreement-details.component.js
@@ -11,29 +11,12 @@
             }
         });
 
-    scientillaAgreementDetails.$inject = ['context', 'AuthService', 'UserService', 'GroupsService'];
+    scientillaAgreementDetails.$inject = [];
 
-    function scientillaAgreementDetails(context, AuthService, UserService, GroupsService) {
+    function scientillaAgreementDetails() {
         const vm = this;
 
-        vm.getAlias = UserService.getAlias;
-        vm.groups = [];
-
-        /* jshint ignore:start */
-        vm.$onInit = async function () {
-            vm.groups = await GroupsService.getGroups();
-        };
-        /* jshint ignore:end */
-
-        vm.getGroupName = function(researchLine) {
-            if (!_.isEmpty(vm.groups)) {
-                const group = vm.groups.find(g => g.code === researchLine.code);
-
-                if (group) {
-                    return group.name;
-                }
-            }
-            return researchLine.description;
+        vm.$onInit = function () {
         };
     }
 
