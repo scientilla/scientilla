48c3eed78240302c5a53c9e00f762d1a444b280d
Dieter Casier <casier.dieter@gmail.com>
2021-04-22 17:57:25 +0200

    Add string to slug function from agreements branche

--- api/services/Utils.js
+++ api/services/Utils.js
@@ -6,13 +6,31 @@ const axios = require('axios');
 const https = require('https');
 const util = require('util');
 const fs = require('fs');
-const { response } = require('express');
 const readFile = util.promisify(fs.readFile);
 
 module.exports = {
+    stringToSlug,
     waitForSuccesfulRequest,
     getActiveDirectoryUsers
 };
+
+function stringToSlug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    const to   = "aaaaeeeeiiiioooouuuunc------";
+    for (let i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
 
 
 async function waitForSuccesfulRequest(options) {
