1cc669c7d8f2648c22e15c1dda72d647972c512a
Dieter Casier <casier.dieter@gmail.com>
2021-05-21 18:01:10 +0200

    Update cid of users when saving cid associations

--- api/controllers/GeneralSettingsController.js
+++ api/controllers/GeneralSettingsController.js
@@ -11,6 +11,22 @@ module.exports = {
         const data = JSON.parse(req.body.data);
         const result = await GeneralSettings.setSetting(name, data);
 
+        switch (name) {
+            case 'cid-associations':
+                await handleCIDAssociations(data);
+            default:
+                break;
+        }
+
         res.halt(Promise.resolve(result));
     },
 };
+
+async function handleCIDAssociations(data) {
+    await data.forEach(async association => {
+        const user = await User.findOne({username: association.email});
+        if (user.cid !== association.cid) {
+            await User.update({id: user.id}, {cid: association.cid});
+        }
+    });
+}
