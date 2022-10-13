10059991af6f17a9b18d1bb6e0f635d16ee3c9af
fedesem2 <fedesem2@yahoo.it>
2022-10-13 20:12:07 +0200

    Improved suggested performance by 40X

--- config/databaseViews/research_item_suggestion.sql
+++ config/databaseViews/research_item_suggestion.sql
@@ -1,59 +1,46 @@
 CREATE OR REPLACE VIEW research_item_suggestion AS
+WITH main_istitute_research_item AS
+         (SELECT *
+          FROM research_item ri
+          WHERE kind = 'v'
+            AND exists(SELECT *
+                       FROM author a
+                                JOIN author_affiliation aa on aa.author = a.id
+                       WHERE a.research_item = ri.id
+                         AND aa.institute = 1))
 SELECT DISTINCT research_item,
                 research_entity
-FROM (
-         SELECT ri.id             AS research_item,
-                u.research_entity AS research_entity,
-                ri.kind
-         FROM "user" u
-                  JOIN research_item ri
-                       ON exists(
-                               SELECT au.id
-                               FROM author au
-                                        JOIN alias al ON lower(au.author_str) = lower(al.str)
-                               WHERE au.research_item = ri.id
-                                 AND al.user = u.id
-                           )
-         UNION
-         SELECT ri.id             AS accomplishment,
-                g.research_entity AS research_entity,
-                ri.kind
-         FROM "group" g
-                  JOIN membership m on g.id = m."group"
-                  JOIN "user" gu on m."user" = gu.id
-                  JOIN research_item ri ON exists(
-                 SELECT guv.id
-                 FROM verify guv
-                 WHERE gu.research_entity = guv.research_entity
-                   AND ri.id = guv.research_item
-             )
-         WHERE exists(SELECT a.id
-                      FROM author a
-                               JOIN author_affiliation aa on aa.author = a.id
-                      WHERE a.research_item = ri.id
-                        AND aa.institute = 1)
-         UNION
-         SELECT ri.id             AS accomplishment,
-                g.research_entity AS research_entity,
-                ri.kind
-         FROM "group" g
-                  JOIN membershipgroup m on g.id = m.parent_group
-                  JOIN "group" g2 on m."child_group" = g2.id
-                  JOIN research_item ri ON exists(
-                 SELECT guv.id
-                 FROM verify guv
-                 WHERE g2.research_entity = guv.research_entity
-                   AND ri.id = guv.research_item
-             )
-         WHERE exists(SELECT a.id
-                      FROM author a
-                               JOIN author_affiliation aa on aa.author = a.id
-                      WHERE a.research_item = ri.id
-                        AND aa.institute = 1)
-     ) suggested
-
-WHERE kind = 'v'
-  AND NOT exists(
+FROM (SELECT ri.id             AS research_item,
+             u.research_entity AS research_entity,
+             ri.kind
+      FROM "user" u
+               JOIN research_item ri
+                    ON exists(
+                            SELECT *
+                            FROM author au
+                                     JOIN alias al ON lower(au.author_str) = lower(al.str)
+                            WHERE au.research_item = ri.id
+                              AND al.user = u.id
+                        )
+      UNION
+      SELECT ri.id             AS research_item,
+             g.research_entity AS research_entity,
+             ri.kind
+      FROM "group" g
+               JOIN membership m on g.id = m."group"
+               JOIN "user" gu on m."user" = gu.id
+               JOIN verify guv on gu.research_entity = guv.research_entity
+               JOIN main_istitute_research_item ri ON ri.id = guv.research_item
+      UNION
+      SELECT ri.id             AS research_item,
+             g.research_entity AS research_entity,
+             ri.kind
+      FROM "group" g
+               JOIN membershipgroup m on g.id = m.parent_group
+               JOIN "group" g2 on m."child_group" = g2.id
+               JOIN verify g2v on g2.research_entity = g2v.research_entity
+               JOIN main_istitute_research_item ri ON ri.id = g2v.research_item) suggested
+WHERE NOT exists(
         SELECT v.id
         FROM verify v
         WHERE v.research_item = suggested.research_item
