f5cb9a6aeb3e1ea008a670d689cf4c458aa53c68
fedesem2 <fedesem2@yahoo.it>
2025-12-09 16:02:31 +0100

    Updated RI view for suggestion including active=true of the members (groups and users)

--- config/databaseViews/research_item_suggestion.sql
+++ config/databaseViews/research_item_suggestion.sql
@@ -15,40 +15,41 @@ FROM (SELECT ri.id             AS research_item,
              ri.kind
       FROM "user" u
                JOIN research_item ri
-                    ON exists(
-                            SELECT *
-                            FROM author au
-                                     JOIN alias al ON lower(au.author_str) = lower(al.str)
-                            WHERE au.research_item = ri.id
-                              AND al.user = u.id
-                        )
+                    ON exists(SELECT *
+                              FROM author au
+                                       JOIN alias al ON lower(au.author_str) = lower(al.str)
+                              WHERE au.research_item = ri.id
+                                AND al.user = u.id)
       UNION
+
       SELECT ri.id             AS research_item,
              g.research_entity AS research_entity,
              ri.kind
       FROM "group" g
-               JOIN membership m on g.id = m."group"
+               JOIN membership m
+                    ON g.id = m."group"
+                        AND m.active = true
                JOIN "user" gu on m."user" = gu.id
                JOIN verify guv on gu.research_entity = guv.research_entity
                JOIN main_istitute_research_item ri ON ri.id = guv.research_item
+
       UNION
+
       SELECT ri.id             AS research_item,
              g.research_entity AS research_entity,
              ri.kind
       FROM "group" g
-               JOIN membershipgroup m on g.id = m.parent_group
+               JOIN membershipgroup m
+                    ON g.id = m.parent_group
+                        AND m.active = true
                JOIN "group" g2 on m."child_group" = g2.id
                JOIN verify g2v on g2.research_entity = g2v.research_entity
                JOIN main_istitute_research_item ri ON ri.id = g2v.research_item) suggested
-WHERE NOT exists(
-        SELECT v.id
-        FROM verify v
-        WHERE v.research_item = suggested.research_item
-          AND v.research_entity = suggested.research_entity
-    )
-  AND NOT exists(
-        SELECT d.id
-        FROM discarded d
-        WHERE d.research_item = suggested.research_item
-          AND d.research_entity = suggested.research_entity
-    );
+WHERE NOT exists(SELECT 1
+                 FROM verify v
+                 WHERE v.research_item = suggested.research_item
+                   AND v.research_entity = suggested.research_entity)
+  AND NOT exists(SELECT 1
+                 FROM discarded d
+                 WHERE d.research_item = suggested.research_item
+                   AND d.research_entity = suggested.research_entity);
